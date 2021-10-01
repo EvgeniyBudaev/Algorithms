cc087121adade598238c902012d3d867ee09fe07
Evgeniy Budaev <budaev.e@gmail.com>
2021-10-01 06:59:27 +0300

    last от yndex

--- leetcode/javascript/utils/mydash/last.js
+++ leetcode/javascript/utils/mydash/last.js
@@ -7,4 +7,13 @@ const last = (array) => {
   return length ? array[length - 1] : undefined;
 };
 
-console.log(last(arr)); // => 1
+console.log(last(arr)); // => 3
+
+function lastYandex(list) {
+        if (!Array.isArray(list)) {
+            return undefined;
+        }
+
+        const length = list.length;
+        return length ? list[length - 1] : undefined;
+}
