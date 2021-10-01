d2d935b76cea763da85999d2e1b2fcae46875115
Evgeniy Budaev <budaev.e@gmail.com>
2021-10-01 21:04:54 +0300

    range от yandex

--- leetcode/javascript/utils/mydash/range.js
+++ leetcode/javascript/utils/mydash/range.js
@@ -216,3 +216,29 @@ console.log(range(1, 4, 0));
 
 console.log(range(0));
 // => []
+
+
+/* Yandex */
+const baseRange = (start, end, step) => {
+  let index = -1;
+  let length = Math.max(Math.ceil((end - start) / (step || 1)), 0);
+  const result = new Array(length);
+
+  while (length--) {
+    result[++index] = start;
+    start += step;
+  }
+
+  return result;
+}
+
+// Проверку на типы данных не добавлял, но студенты должны будут
+function range(start = 0, end, step) {
+  if (end === undefined) {
+    end = start;
+    start = 0;
+  }
+
+  step = step === undefined ? (start < end ? 1 : -1) : step;
+  return baseRange(start, end, step);
+}
