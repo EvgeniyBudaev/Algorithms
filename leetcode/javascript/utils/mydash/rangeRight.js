8b0f852102e5635c099b49e3ce92c2805aae6814
Evgeniy Budaev <budaev.e@gmail.com>
2021-10-01 21:48:36 +0300

    rangeRight от Yandex

--- leetcode/javascript/utils/mydash/rangeRight.js
+++ leetcode/javascript/utils/mydash/rangeRight.js
@@ -199,3 +199,33 @@ console.log(rangeRight(0, 20, 5));  // => [15, 10, 5, 0]
 console.log(rangeRight(0, -4, -1));  // => [-3, -2, -1, 0]
 console.log(rangeRight(1, 4, 0));  // => [1, 1, 1]
 console.log(rangeRight(0));  // => []
+
+
+/* Yandex решение */
+const baseRange = (start, end, step, isRight) => {
+  let index = -1;
+  let length = Math.max(Math.ceil((end - start) / (step || 1)), 0);
+  const result = new Array(length);
+
+  while (length--) {
+    result[isRight ? length : ++index] = start;
+    start += step;
+  }
+
+  return result;
+}
+
+// Проверку на типы данных не добавлял, но студенты должны будут
+function range(start = 0, end, step, isRight = false) {
+        if (end === undefined) {
+      end = start;
+            start = 0;
+    }
+
+    step = step === undefined ? (start < end ? 1 : -1) : step;
+    return baseRange(start, end, step, isRight);
+}
+
+function rangeRight(start, end, step) {
+        return range(start, end, step, true);
+}
