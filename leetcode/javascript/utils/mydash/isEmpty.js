cb819964792a646619169a18b73200724c04b70b
Evgeniy Budaev <budaev.e@gmail.com>
2021-10-29 17:00:21 +0300

    isNil

--- leetcode/javascript/utils/mydash/isEmpty.js
+++ leetcode/javascript/utils/mydash/isEmpty.js
@@ -257,3 +257,80 @@ console.log(isEmpty(true));  // => true
 console.log(isEmpty(1));  // => true
 console.log(isEmpty([1, 2, 3]));  // => false
 console.log(isEmpty({ 'a': 1 }));  // => false
+
+/* Yandex решение */
+function isLength(value) {
+  return (
+    typeof value === "number" &&
+    value > -1 &&
+    value % 1 === 0 &&
+    value <= Number.MAX_SAFE_INTEGER
+  );
+}
+
+function isNil(value) {
+  return value === null || value === undefined;
+}
+
+function isArrayLike(value) {
+  return !isNil(value) && typeof value !== "function" && isLength(value.length);
+}
+
+function isObjectLike(value) {
+  return typeof value === "object" && value !== null;
+}
+
+function getTag(value) {
+  if (value === null) {
+    return value === undefined ? "[object Undefined]" : "[object Null]";
+  }
+  return toString.call(value);
+}
+
+const objectProto = Object.prototype;
+function isPrototype(value) {
+  const ctor = value && value.constructor;
+  const proto = (typeof ctor === "function" && ctor.prototype) || objectProto;
+
+  return value === proto;
+}
+
+function isArguments(value) {
+  return isObjectLike(value) && getTag(value) === "[object Arguments]";
+}
+
+// Реализация лодаша
+function isEmpty(value) {
+  if (value === null) {
+    return true;
+  }
+
+  if (
+    isArrayLike(value) &&
+    (Array.isArray(value) ||
+      typeof value === "string" ||
+      typeof value.splice === "function" ||
+      isBuffer(value) ||
+      isTypedArray(value) ||
+      isArguments(value))
+  ) {
+    return !value.length;
+  }
+
+  const tag = getTag(value);
+  if (tag === "[object Map]" || tag === "[object Set]") {
+    return !value.size;
+  }
+
+  if (isPrototype(value)) {
+    return !Object.keys(value).length;
+  }
+
+  for (const key in value) {
+    if (hasOwnProperty.call(value, key)) {
+      return false;
+    }
+  }
+
+  return true;
+}
