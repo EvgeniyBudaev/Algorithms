54ba321c3f1c41f39fc1772e960bbcf7a7ee2023
Evgeniy Budaev <budaev.e@gmail.com>
2022-04-04 18:46:10 +0300

    Tribonacci Sequence

---
+++ codewars/tribonacciSequence/tribonacciSequence.js
@@ -0,0 +1,14 @@
+function tribonacci(signature,n){
+  if (n === 0) return [];
+  if (n === 1) return [signature[0]];
+  if (n === 2) return [signature[0], signature[1]];
+  if (n === 3) return [signature[0], signature[1], signature[2]];
+  let tribonacciArray = [signature[0], signature[1], signature[2]];
+  for (let i = 3; i < n; i++) {
+	tribonacciArray.push(tribonacciArray[i - 1] + tribonacciArray[i - 2] + tribonacciArray[i - 3]);
+  }
+  return tribonacciArray;
+}
+
+
+console.log(tribonacci([1,1,1],10)); // [1,1,1,3,5,9,17,31,57,105]
