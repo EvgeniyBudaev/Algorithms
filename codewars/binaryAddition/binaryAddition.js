9ce20d6548731279a0e70c9d329d5aa44aa0cab2
Evgeniy Budaev <budaev.e@gmail.com>
2022-04-04 10:59:55 +0300

    Binary Addition

---
+++ codewars/binaryAddition/binaryAddition.js
@@ -0,0 +1,8 @@
+/* Функция addBinary преобразует числа в двоичный формат */
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+console.log(addBinary(1, 1));
+console.log(addBinary(5, 9));
