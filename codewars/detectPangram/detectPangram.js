d73907328056f4fb7985105800eddf9357b2c585
Evgeniy Budaev <budaev.e@gmail.com>
2022-04-04 07:39:58 +0300

    Панграмма

---
+++ codewars/detectPangram/detectPangram.js
@@ -0,0 +1,21 @@
+/* Панграмма — это предложение, в котором каждая буква алфавита встречается хотя
+ бы по одному разу. */
+
+function isPangram(string) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	return [...alphabet].every(letter => string.toLowerCase().includes(letter));
+}
+
+// function isPangram(string){
+// 	const REGEX = /([a-z])(?!.*\1)/g;
+// 	return (string.toLowerCase().match(REGEX) || []).length === 26;
+// }
+
+// function isPangram(string) {
+// 	const alphabetList = [...'abcdefghijklmnopqrstuvwxyz'];
+// 	return alphabetList.every((letter) => string.toLowerCase().includes(letter));
+// }
+
+console.log(isPangram('The quick brown fox jumps over the lazy dog.')); // true
+console.log(isPangram('This is not a pangram.')); // false
+console.log(isPangram('Cwm fjord bank glyphs vext quiz is a pangram')); // true
