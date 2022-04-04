a5009c3f1e55bd1d0e805809a41fb0ef62a36b40
Evgeniy Budaev <budaev.e@gmail.com>
2022-04-04 20:04:06 +0300

    Find the unique number

--- codewars/findUniqueNumber/findUniqueNumber.js
+++ codewars/findUniqueNumber/findUniqueNumber.js
@@ -1,7 +1,7 @@
 /* Функция findUniq находит уникальное значение в массиве */
 
 function findUniq(arr) {
-  return arr.filter(item=> arr.indexOf(item) === arr.lastIndexOf(item))[0];
+  return arr.filter(item => arr.indexOf(item) === arr.lastIndexOf(item))[0];
 }
 
 console.log(findUniq([ 1, 1, 1, 2, 1, 1 ])); // 2
