68b3734eee6e761ee927e110d77066f9ca8e722e
Evgeniy Budaev <budaev.e@gmail.com>
2022-04-15 20:23:38 +0300

    Lerua Merlen

---
+++ leruaMerlen/leruaMerlen.js
@@ -0,0 +1,39 @@
+/*
+Марсоход проезжает на каждой итерации на 1метр
+l-налево, r-направо, t-вверх, b-вниз.
+Если марсоход поехал налево, а затем направо, то он остался на той же позиции и
+в итоге нужно вернуть пустой массив. В ином случае нужно вернуть массив
+элементов, в какую сторону поехал марсоход.
+*/
+
+// ['l', 'r'] => []
+// ['l', 'r', 't', 'b'] => []
+// ['l', 'l', 'l', 'r', 't', 'b'] => ['l', 'l']
+
+const moveOptions = {
+  l: 'r',
+  r: 'l',
+  t: 'b',
+  b: 't',
+};
+
+const getCommon = (list) => {
+  const common = [];
+
+  list.forEach(item => {
+    const lastItem = common[common.length - 1];
+
+    if (lastItem !== moveOptions[item]) {
+      common.push(item);
+    } else {
+      common.pop();
+    }
+  });
+
+  return common;
+};
+
+// console.log(getCommon(['l', 'r']));
+// console.log(getCommon(['l', 'r', 't', 'b']));
+// console.log(getCommon(['l', 'l']));
+console.log(getCommon(['l', 'l', 'l', 'r', 't', 'b'])); // ['l', 'l']
