52ee70e0d5fd351ebc7d2d67554cfbd58273612a
Evgeniy Budaev <budaev.e@gmail.com>
2022-05-31 10:54:27 +0300

    Две фишки || Two sum

---
+++ yandex/dveFishki/index.js
@@ -0,0 +1,30 @@
+/* Нужно вывести два числа —– очки на двух фишках, в сумме дающие k.
+Если таких пар несколько, то можно вывести любую из них.
+Если таких пар не существует, то вывести [].  */
+
+const initialData = [-1, -9, -7, 3, -6]; // [-1, 3]
+// const initialData = [-93, 67, 84, -30, -96, 22, 40, -11, -39, 11]; // None
+
+function twoSum(array, targetSum) {
+	const result = [];
+
+	for (let i = 0; i < array.length; i++) {
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[i] + array[j] === targetSum) {
+				result.push([array[i], array[j]]);
+			}
+		}
+	}
+	if (result.length > 0) {
+		if (result.length === 1) {
+			return result[0];
+		} else {
+			return result[Math.floor(Math.random() * result.length)];
+		}
+	} else {
+		return [];
+	}
+}
+
+console.log(twoSum(initialData, 2));
+// console.log(twoSum(initialData, -186));
