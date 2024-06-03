be0a7c1d0d0c5952871fdd7ba3eb985ef21ca917
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-03 20:45:17 +0300

    Duplicate Zeros

--- leetcode/javascript/array/duplicateZeros/duplicateZeros.js
+++ leetcode/javascript/array/duplicateZeros/duplicateZeros.js
@@ -17,13 +17,10 @@ const arr = [1,0,2,3,0,4,5,0];
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-    let length = arr.length;
-
-    for (let i = 0; i < length; i++) {
-        if(arr[i] === 0) {
-            for (let j = length - 1; j > i; j--) {
-                arr[j] = arr[j-1];
-            }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            arr.pop();
             i++;
         }
     }
