7f3b1d1d1df3cfcac6677ce37267e3302cdbcdea
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-04 20:26:55 +0300

    Check If Palindrome

---
+++ leetcode/javascript/array/TwoPointers/checkIfPalindrome/checkIfPalindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkIfPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(checkIfPalindrome("racecar"));
+console.log(checkIfPalindrome("aleba"));
