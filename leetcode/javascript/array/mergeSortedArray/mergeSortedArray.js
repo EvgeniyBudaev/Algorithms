80a292715f235c02a3ae2cc5c59877182a7c9437
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-04 06:15:21 +0300

    Merge Sorted Array

---
+++ leetcode/javascript/array/mergeSortedArray/mergeSortedArray.js
@@ -0,0 +1,35 @@
+/*
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+*/
+/*
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+*/
+/*
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; i < m + n; i++, j++) {
+        nums1[i] = nums2[j];
+    }
+    return nums1.sort((a, b) => a - b);
+};
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
