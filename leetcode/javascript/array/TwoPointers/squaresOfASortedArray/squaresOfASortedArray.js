e2d3996a85548f1f63229b3a83cfdfa8269b2415
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-05 08:03:46 +0300

    Squares of a Sorted Array

---
+++ leetcode/javascript/array/TwoPointers/squaresOfASortedArray/squaresOfASortedArray.js
@@ -0,0 +1,35 @@
+/*
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+ */
+
+/*
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let result = [];
+
+    for (let i = right; i >= 0; i--) {
+        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+            result[i] = nums[right] ** 2;
+            right--;
+        } else {
+            result[i] = nums[left] ** 2;
+            left++;
+        }
+    }
+
+    return result;
+};
+
+console.log(sortedSquares([-4,-1,0,3,10]));
