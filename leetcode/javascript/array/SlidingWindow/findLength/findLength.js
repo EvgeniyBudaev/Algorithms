6801629775d7fbd38008e8f453fc118b1461deb2
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-06 07:06:05 +0300

    Find Length

---
+++ leetcode/javascript/array/SlidingWindow/findLength/findLength.js
@@ -0,0 +1,32 @@
+/*
+Дан массив целых положительных чисел nums и целое число k. Найдите длину самого длинного подмассива, сумма которого
+меньше или равна k.
+ */
+
+/*
+Input: nums = [3, 1, 2, 7, 4, 2, 1, 1, 5], k = 8
+Output: ["o","l","l","e","h"]
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findLength = function(nums, k) {
+    // curr is the current sum of the window
+    let left = 0, curr = 0, ans = 0;
+    for (let right = 0; right < nums.length; right++) {
+        curr += nums[right];
+        while (curr > k) {
+            curr -= nums[left];
+            left++;
+        }
+
+        ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+}
+
+console.log(findLength([3, 1, 2, 7, 4, 2, 1, 1, 5], 8));// 4
