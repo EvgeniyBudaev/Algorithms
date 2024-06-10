224af80d16838b1a2c9192f42f3ed1f19769f28a
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-10 08:31:37 +0300

    1832. Check if the Sentence Is Pangram

---
+++ leetcode/javascript/hashing/checkIfTheSentenceIsPangram/checkIfTheSentenceIsPangram.js
@@ -0,0 +1,29 @@
+/*
+Панграмма – это предложение, в котором каждая буква английского алфавита встречается хотя бы один раз.
+
+Учитывая строковое предложение, содержащее только строчные буквы английского языка, верните true, если предложение
+является панграммой, или false в противном случае.
+ */
+
+/*
+Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+Output: true
+ */
+
+/*
+Input: sentence = "leetcode"
+Output: false
+ */
+
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    if (sentence.length < 26) return false;
+    const arr = sentence.split('');
+    const set = new Set(arr);
+    return set.size === 26;
+};
+
+console.log(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
