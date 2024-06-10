2f097c9a31e02e08ec539ca5849098a218479c19
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-10 08:55:51 +0300

    split

---
+++ leetcode/javascript/utils/split.js
@@ -0,0 +1,18 @@
+export function split(str, separator) {
+    const res = [];
+    let temp = '';
+    for(let i = 0; i < str.length; i++){
+        const el = str[i];
+        if(el === separator || separator === '' && temp){
+            res.push(temp);
+            temp = '';
+        }
+        if(el !== separator){
+            temp += el;
+        }
+    }
+    if(temp){
+        res.push(temp);
+    }
+    return res;
+}
