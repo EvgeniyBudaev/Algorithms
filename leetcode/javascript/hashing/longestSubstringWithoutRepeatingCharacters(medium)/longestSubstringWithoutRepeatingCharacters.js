605307fb3bfa1fddd0af90a2a22d245b1ee6deb1
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-12 11:25:33 +0300

    3. Longest Substring Without Repeating Characters

---
+++ leetcode/javascript/hashing/longestSubstringWithoutRepeatingCharacters(medium)/longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,46 @@
+/*
+Дана строка s, найдите длину самой длинной подстроки без повторения символов.
+ */
+
+/*
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+ */
+
+/*
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+ */
+
+/*
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let left = 0;
+    let maxLength = 0;
+    let charSet = new Set();
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
