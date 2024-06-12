71cf3ade02af5c270adff8c8ce798ebad937fc91
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-12 10:22:39 +0300

    383. Ransom Note

---
+++ leetcode/javascript/hashing/ransomNote(easy)/ransomNote.js
@@ -0,0 +1,53 @@
+/*
+Учитывая две строки ransomNote и magazine, верните true, если ransomNote можно создать с использованием букв из magazine,
+и false в противном случае.
+
+Каждое письмо в magazine можно использовать в ransomNote только один раз.
+ */
+
+/*
+Input: ransomNote = "a", magazine = "b"
+Output: false
+ */
+
+/*
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+ */
+
+/*
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ */
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    for(let i= 0; i < ransomNote.length; i++){
+        if (!magazine.includes(ransomNote[i])) return false;
+        magazine = magazine.replace(ransomNote[i],"");
+    }
+
+    return true;
+};
+
+// var canConstruct = function(ransomNote, magazine) {
+//     const map = {};
+//
+//     for(const letter of magazine) {
+//         map[letter] = map[letter] + 1 || 1;
+//     }
+//
+//     for(let letter of ransomNote) {
+//         if(!map[letter]) {
+//             return false;
+//         }
+//         map[letter]--;
+//     }
+//     return true;
+// };
+
+console.log(canConstruct("aa", "ab")); // false
