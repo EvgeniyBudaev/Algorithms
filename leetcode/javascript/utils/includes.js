c34fd5ff565d28e7791217be6aeab435b6e3af13
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-12 11:05:29 +0300

    includes

--- leetcode/javascript/utils/includes.js
+++ leetcode/javascript/utils/includes.js
@@ -1,9 +1,6 @@
 export function includes(array, searchString) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === searchString) {
-            return true;
-        }
+        if (array[i] === searchString) return true;
     }
-
     return false;
 }
