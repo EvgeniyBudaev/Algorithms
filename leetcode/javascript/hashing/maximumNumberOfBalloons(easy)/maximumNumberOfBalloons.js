ef54101735048c791d67415f93d03f8cb9faef3c
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-12 09:01:01 +0300

    1189. Maximum Number of Balloons

---
+++ leetcode/javascript/hashing/maximumNumberOfBalloons(easy)/maximumNumberOfBalloons.js
@@ -0,0 +1,42 @@
+/*
+Учитывая строковый текст, вы хотите использовать символы текста для формирования как можно большего количества
+экземпляров слова «balloon».
+
+Каждый символ в тексте можно использовать не более одного раза. Возвращает максимальное количество экземпляров,
+которые можно сформировать.
+ */
+
+/*
+Input: text = "nlaebolko"
+Output: 1
+ */
+
+/*
+Input: text = "loonbalxballpoon"
+Output: 2
+ */
+
+/*
+Input: text = "leetcode"
+Output: 0
+ */
+
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = function(text) {
+    const map = { b: 0, a: 0, l: 0, o: 0, n: 0, };
+
+    for(const char of text) {
+        map[char]++;
+    }
+
+    return Math.floor(
+        Math.min(map.b, map.a, map.l / 2, map.o / 2, map.n)
+    );
+};
+
+console.log(maxNumberOfBalloons("nlaebolko")); // 1
+console.log(maxNumberOfBalloons("loonbalxballpoon")); // 2
+console.log(maxNumberOfBalloons("leetcode")); // 0
