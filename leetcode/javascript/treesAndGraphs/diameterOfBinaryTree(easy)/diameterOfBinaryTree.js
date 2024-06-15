e6c4d811db99d38e9d84964771ba4bd31c363c03
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-15 12:17:35 +0300

    543. Diameter of Binary Tree

--- leetcode/javascript/treesAndGraphs/diameterOfBinaryTree(easy)/diameterOfBinaryTree.js
+++ leetcode/javascript/treesAndGraphs/diameterOfBinaryTree(easy)/diameterOfBinaryTree.js
@@ -60,7 +60,6 @@ var diameterOfBinaryTree = function(root) {
     let diameter = 0;
 
     function dfs(node) {
-        console.log("node:", node);
         if (!node) return 0;
         let left = dfs(node.left);
         let right = dfs(node.right);
