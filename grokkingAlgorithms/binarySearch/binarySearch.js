a5715c9221c35a1b550ead90db1d3252e4bb0e11
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-19 13:18:25 +0300

    binary search

---
+++ grokkingAlgorithms/binarySearch/binarySearch.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} list
+ * @param {number} item
+ * @return {number|null}
+ */
+const binarySearch = (list, item) => {
+    let low = 0;
+    let high = list.length - 1;
+
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        const guess = list[mid]; // guess - предполагаемое число
+
+        if (guess === item) return mid;
+
+        if (guess > item) high = mid - 1;
+        else high = mid + 1;
+    }
+
+    return null;
+}
+
+const myList = [1, 3, 5, 7, 9];
+
+console.log(binarySearch(myList, 3)); // 1
+console.log(binarySearch(myList, -1)); // null
