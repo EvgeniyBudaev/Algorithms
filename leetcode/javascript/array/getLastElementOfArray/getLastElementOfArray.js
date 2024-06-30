356bec4d9d762003466e33591006a7c74af8d8b1
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-30 19:57:12 +0300

    getLastElementOfArray

--- leetcode/javascript/array/getLastElementOfArray/getLastElementOfArray.js
+++ leetcode/javascript/array/getLastElementOfArray/getLastElementOfArray.js
@@ -5,10 +5,8 @@
 
 var words = ['Ночь', 'Улица', 'Фонарь', 'Аптека', 'Бессмысленный', 'Тусклый', 'Свет'];
 
-getLastElementOfArray(words); // возвращает Свет;
-
 function getLastElementOfArray(array) {
-  // Напишите код здесь
-  const result = array[array.length -1];
-  console.log(result);
+  return array[array.length - 1];
 }
+
+console.log(getLastElementOfArray(words)); // возвращает Свет;
