23c2e87df7d34cc1555336e01c041c0c492e4157
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-30 20:25:11 +0300

    88. Merge Sorted Array

--- leetcode/javascript/array/insertingItemsIntoAnArray/mergeSortedArray.js
+++ leetcode/javascript/array/insertingItemsIntoAnArray/mergeSortedArray.js
@@ -1,11 +1,14 @@
-/*You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
-representing the number of elements in nums1 and nums2 respectively.
+/*
+Вам даны два целочисленных массива nums1 и nums2, отсортированные в порядке неубывания, и два целых числа m и n,
+представляющее количество элементов в nums1 и nums2 соответственно.
 
-Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+Объедините nums1 и nums2 в один массив, отсортированный в неубывающем порядке.
 
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
-To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should 
-be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n. */
+Окончательно отсортированный массив не должен возвращаться функцией, а должен храниться внутри массива nums1.
+Для этого nums1 имеет длину m + n, где первые m элементов обозначают элементы, которые должны
+быть объединены, а последние n элементов установлены в 0 и их следует игнорировать. nums2 имеет длину n.
+*/
+
 /*
 Example 1:
 Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
@@ -34,13 +37,21 @@ nums2.length == n
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+// var merge = function(nums1, m, nums2, n) {
+//     for (let i = m, j = 0; i < m + n; i++, j++) {
+//         nums1[i] = nums2[j];
+//     }
+//     nums1.sort((a, b) => a - b);
+//     console.log(nums1);
+// };
+
  const merge = function(nums1, m, nums2, n) {
     let p1 = m - 1;
     let p2 = n - 1;
 
     for (let i = m + n - 1; i >= 0; i--) {
         if (p2 < 0) break;
-        
+
         if (nums1[p1] > nums2[p2]) {
             nums1[i] = nums1[p1];
             p1--;
@@ -49,6 +60,8 @@ nums2.length == n
             p2--;
         }
     }
+
+    console.log(nums1);
 };
 
-console.log(merge([1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3));
+merge([1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3);
