ee5e7e2ac20401556ab58dd32a138fed02a6326e
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-30 17:06:23 +0300

    doubleNumber

--- leetcode/javascript/array/doubleNumber/doubleNumber.js
+++ leetcode/javascript/array/doubleNumber/doubleNumber.js
@@ -1,9 +1,9 @@
 const arr = [17, 23, 31, 44, 59];
 
-doubleNumber(arr); // возвращает [34,46,62,88,118];
-
 function doubleNumber(array) {
   // Ваш код
   const res = array.map(item => item * 2);
-  console.log(res)
-};
+  console.log(res);
+}
+
+doubleNumber(arr); // возвращает [34,46,62,88,118];
