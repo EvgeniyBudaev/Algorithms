fd59decd940403a3a31ad1afc10d73b37c76fe6b
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-30 20:00:31 +0300

    1089. Duplicate Zeros

--- leetcode/javascript/array/duplicateZeros/duplicateZeros.js
+++ leetcode/javascript/array/duplicateZeros/duplicateZeros.js
@@ -12,6 +12,7 @@ Input: arr = [1,0,2,3,0,4,5,0]
 Output: [1,0,0,2,3,0,0,4]
 Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
 */
+
 /*
 Input: arr = [1,2,3]
 Output: [1,2,3]
--- leetcode/javascript/array/insertingItemsIntoAnArray/duplicateZeros.js
+++ leetcode/javascript/array/insertingItemsIntoAnArray/duplicateZeros.js
@@ -1,20 +1,22 @@
-/* Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right. */
-/* Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything. */
-/* 
-Example 1:
+// https://leetcode.com/problems/duplicate-zeros/description/
+
+/*
+Учитывая целочисленный массив фиксированной длины arr, дублируйте каждое вхождение нуля, сдвигая оставшиеся элементы
+вправо.
+Обратите внимание, что элементы, превышающие длину исходного массива, не записываются. Внесите вышеуказанные изменения
+во входной массив и ничего не возвращайте.
+*/
+
+/*
 Input: arr = [1,0,2,3,0,4,5,0]
 Output: [1,0,0,2,3,0,0,4]
 Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+*/
 
-Example 2:
+/*
 Input: arr = [1,2,3]
 Output: [1,2,3]
 Explanation: After calling your function, the input array is modified to: [1,2,3]
-
-Constraints:
-
-1 <= arr.length <= 104
-0 <= arr[i] <= 9
 */
 
 /**
@@ -29,7 +31,6 @@ Constraints:
             arr.pop();
         }    
     }
-    return arr;
 };
 
-console.log(duplicateZeros([1,0,2,3,0,4,5,0]));
+duplicateZeros([1,0,2,3,0,4,5,0]);
