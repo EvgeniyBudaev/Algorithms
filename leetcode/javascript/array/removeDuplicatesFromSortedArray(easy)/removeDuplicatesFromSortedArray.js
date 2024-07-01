97fbcfa8b540e3b192a2943cea49f8a10002ab01
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-01 18:31:23 +0300

    26. Remove Duplicates from Sorted Array

--- leetcode/javascript/array/removeDuplicatesFromSortedArray(easy)/removeDuplicatesFromSortedArray.js
+++ leetcode/javascript/array/removeDuplicatesFromSortedArray(easy)/removeDuplicatesFromSortedArray.js
@@ -55,5 +55,4 @@ var removeDuplicates = function(nums) {
 //     return nums;
 // };
 
-//console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
-console.log(removeDuplicates([0,0,1]));
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
