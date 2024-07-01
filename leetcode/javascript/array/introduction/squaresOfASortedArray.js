d9a42ea9e74274c88a5fbeaa860be38b350fbb3d
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-01 08:08:41 +0300

    977. Squares of a Sorted Array

--- leetcode/javascript/array/introduction/squaresOfASortedArray.js
+++ leetcode/javascript/array/introduction/squaresOfASortedArray.js
@@ -1,7 +1,14 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/description/
 /* Squares of a Sorted Array */
-/* Квадраты отсортированного массива */
+
+/* Two pointers
+Time complexity: O(n)
+Space complexity: O(n)
+ */
+
 /* 
-Учитывая целочисленный массив nums, отсортированный в неубывающем порядке, вернуть массив квадратов каждого числа, отсортированных в неубывающем порядке.
+Учитывая целочисленный массив nums, отсортированный в неубывающем порядке, вернуть массив квадратов каждого числа,
+отсортированных в неубывающем порядке.
 
 Example 1:
 Input: nums = [-4,-1,0,3,10]
@@ -16,23 +23,36 @@ Output: [4,9,9,49,121]
  * @param {number[]} nums
  * @return {number[]}
  */
+const sortedSquares = function(nums){
+    let res = new Array(nums.length).fill(0);
+    let left = 0;
+    let right = nums.length - 1;
 
-const compareNumbers = (a, b) => {
-    return a - b;
-};
-
-const sortedSquares = function(nums) {
-    const listPositive = nums.map(item => {
-        if (item < 0) {
-            return -1 * item;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+            res[i] = nums[left] ** 2;
+            left++;
         } else {
-            return item;
+            res[i] = nums[right] ** 2;
+            right--;
         }
-    }); 
+    }
 
-    const listSorted = listPositive.sort(compareNumbers); 
-    return listSorted.map(item => item * item);
-};
+    return res;
+}
+
+// const sortedSquares = function(nums) {
+//     const listPositive = nums.map(item => {
+//         if (item < 0) {
+//             return -1 * item;
+//         } else {
+//             return item;
+//         }
+//     });
+//
+//     const listSorted = listPositive.sort((a, b) => a - b);
+//     return listSorted.map(item => item * item);
+// };
 
 const nums = [-4, -1, 0, 3, 10];
 console.log(sortedSquares(nums));
