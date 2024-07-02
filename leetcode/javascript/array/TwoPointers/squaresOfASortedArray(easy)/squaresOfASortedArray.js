8770f6c6a55e6f9e42b8b4a6955b820e108ecd23
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-02 11:17:59 +0300

    977. Squares of a Sorted Array

--- leetcode/javascript/array/TwoPointers/squaresOfASortedArray(easy)/squaresOfASortedArray.js
+++ leetcode/javascript/array/TwoPointers/squaresOfASortedArray(easy)/squaresOfASortedArray.js
@@ -1,16 +1,13 @@
-/*
+/* https://leetcode.com/problems/squares-of-a-sorted-array/description/
+
 Учитывая целочисленный массив nums, отсортированный в неубывающем порядке, верните массив квадратов каждого числа,
 отсортированного в неубывающем порядке.
- */
 
-/*
 Input: nums = [-4,-1,0,3,10]
 Output: [0,1,9,16,100]
 Explanation: After squaring, the array becomes [16,1,0,9,100].
 After sorting, it becomes [0,1,9,16,100].
- */
 
-/*
 Input: nums = [-7,-3,2,3,11]
 Output: [4,9,9,49,121]
  */
@@ -20,19 +17,15 @@ Output: [4,9,9,49,121]
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    let left = 0;
-    let right = nums.length - 1;
-    let idx = right;
+    let left = 0, right = nums.length - 1;
     let result = [];
 
-    while (idx > -1) {
+    for (let i = nums.length - 1; i >= 0; i--) {
         if (Math.abs(nums[left]) < Math.abs(nums[right])) {
-            result[idx] = nums[right] ** 2;
-            idx--;
+            result[i] = nums[right] ** 2;
             right--;
         } else {
-            result[idx] = nums[left] ** 2;
-            idx--;
+            result[i] = nums[left] ** 2;
             left++;
         }
     }
@@ -40,22 +33,4 @@ var sortedSquares = function(nums) {
     return result;
 };
 
-// var sortedSquares = function(nums) {
-//     let left = 0;
-//     let right = nums.length - 1;
-//     let result = [];
-//
-//     for (let i = right; i >= 0; i--) {
-//         if (Math.abs(nums[left]) < Math.abs(nums[right])) {
-//             result[i] = nums[right] ** 2;
-//             right--;
-//         } else {
-//             result[i] = nums[left] ** 2;
-//             left++;
-//         }
-//     }
-//
-//     return result;
-// };
-
-console.log(sortedSquares([-4,-1,0,3,10]));
+console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0, 1, 9, 16, 100]
