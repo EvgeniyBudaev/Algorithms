5d9abf8a4f2f3134fdd98729c232ad1c475087ae
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-02 08:02:59 +0300

    separateArray

--- leetcode/javascript/array/separateArray(easy)/separateArray.js
+++ leetcode/javascript/array/separateArray(easy)/separateArray.js
@@ -1,12 +1,4 @@
-/* */
-
-const numbers = [1, 2, 3, 4, 5, 6];
-
-const result = separateArray(numbers);
-// должен вернуть { even: [2, 4, 6], odd: [1, 3, 5] }
-
 function separateArray(array) {
-  // Ваш код здесь
   const even = [];
   const odd = [];
   array.forEach(item => {
@@ -19,4 +11,5 @@ function separateArray(array) {
   return { even: even, odd: odd };
 }
 
-console.log("Результат: ", result);
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(separateArray(numbers)); // { even: [2, 4, 6], odd: [1, 3, 5] }
