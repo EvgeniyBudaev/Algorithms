5bbeb31a97bb803877032246c928f7feb9e39129
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-02 07:57:34 +0300

    88. Merge Sorted Array

--- leetcode/javascript/array/mergeSortedArray(easy)/mergeSortedArray.js
+++ leetcode/javascript/array/mergeSortedArray(easy)/mergeSortedArray.js
@@ -57,4 +57,12 @@ var merge = function(nums1, m, nums2, n) {
     console.log(nums1);
 };
 
+// var merge = function(nums1, m, nums2, n) {
+//     for (let i = m, j = 0; i < m + n; i++, j++) {
+//         nums1[i] = nums2[j];
+//     }
+//     nums1.sort((a, b) => a - b);
+//     console.log(nums1);
+// };
+
 merge([1,2,3,0,0,0], 3, [2,5,6], 3);
