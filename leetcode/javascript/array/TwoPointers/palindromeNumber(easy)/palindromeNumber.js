1a523b29e6920a023cc26fa8d8fe9f4790551670
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-02 10:05:59 +0300

    9. Palindrome Number

---
+++ leetcode/javascript/array/TwoPointers/palindromeNumber(easy)/palindromeNumber.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/palindrome-number/description/
+
+/*
+Учитывая целое число x, верните true, если x является палиндром и false в противном случае.
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str = String(x);
+    let left = 0, right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(isPalindrome(121));
