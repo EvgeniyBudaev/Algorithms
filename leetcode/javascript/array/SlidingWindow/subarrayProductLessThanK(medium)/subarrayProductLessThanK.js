bbcffbed19bc4c871ea57712139b2914d034ade8
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-03 09:34:34 +0300

    643. Maximum Average Subarray I

--- leetcode/javascript/array/SlidingWindow/subarrayProductLessThanK(medium)/subarrayProductLessThanK.js
+++ leetcode/javascript/array/SlidingWindow/subarrayProductLessThanK(medium)/subarrayProductLessThanK.js
@@ -1,10 +1,15 @@
-/*
+/* https://leetcode.com/problems/subarray-product-less-than-k/description/
 Учитывая массив положительных целых чисел nums и целое число k, верните количество подмассивов, в которых произведение
 всех элементов в подмассиве строго меньше k.
 
-Например, учитывая входные числа = [10, 5, 2, 6], k = 100, ответ — 8. Подмассивы с продуктами меньше k:
-
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Пояснение: 8 подмассивов с произведением меньше 100:
 [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Обратите внимание, что [10, 5, 2] не включены, поскольку произведение 100 не строго меньше k.
+
+Input: nums = [1,2,3], k = 0
+Output: 0
  */
 
 /**
@@ -13,10 +18,7 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) {
-    if (k <= 1) {
-        return 0;
-    }
-
+    if (k <= 1) return 0;
     let ans = 0, left = 0, curr = 1;
 
     for (let right = 0; right < nums.length; right++) {
@@ -25,7 +27,6 @@ var numSubarrayProductLessThanK = function(nums, k) {
             curr /= nums[left];
             left++;
         }
-
         ans += right - left + 1;
     }
 
