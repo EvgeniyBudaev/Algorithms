39894f7be8c658da57364c84b3aa4c8995923a36
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-03 19:34:24 +0300

    1480. Running Sum of 1d Array

--- leetcode/javascript/array/PrefixSum/runningSumOf1dArray/runningSumOf1dArray.js
+++ leetcode/javascript/array/PrefixSum/runningSumOf1dArray/runningSumOf1dArray.js
@@ -1,22 +1,17 @@
-// https://www.youtube.com/watch?v=at9xNhYlXJ4&ab_channel=DevGym Объяснение Prefix Sum
-/*
+/* https://leetcode.com/problems/running-sum-of-1d-array/description/
+https://www.youtube.com/watch?v=at9xNhYlXJ4&ab_channel=DevGym Объяснение Prefix Sum
+
 Дан массив чисел. Мы определяем текущую сумму массива как RunningSum[i] = sum(nums[0]…nums[i]).
 Верните текущую сумму чисел.
- */
 
-/*
 Input: nums = [1,2,3,4]
 Output: [1,3,6,10]
 Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
- */
 
-/*
 Input: nums = [1,1,1,1,1]
 Output: [1,2,3,4,5]
 Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
- */
 
-/*
 Input: nums = [3,1,2,10,1]
 Output: [3,4,6,16,17]
  */
