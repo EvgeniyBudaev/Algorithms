1af3aeb96ae6499cc14cbe12f394fa39e3a6b842
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-04 08:38:27 +0300

    2389. Longest Subsequence With Limited Sum

--- leetcode/javascript/binarySearch/longestSubsequenceWithLimitedSum(easy)/longestSubsequenceWithLimitedSum.js
+++ leetcode/javascript/binarySearch/longestSubsequenceWithLimitedSum(easy)/longestSubsequenceWithLimitedSum.js
@@ -1,12 +1,10 @@
-/*
-Вам дан целочисленный массив nums длины n и запросы целочисленного массива длиной m.
+/* https://leetcode.com/problems/longest-subsequence-with-limited-sum/description/
+Вам дан целочисленный массив nums длины n и запросы целочисленного массива queries длиной m.
 Верните answer в виде массива длиной m, где answer[i] — максимальный размер подпоследовательности, которую можно взять
 из чисел, чтобы сумма ее элементов была меньше или равна запросу [i].
 Подпоследовательность — это массив, который можно получить из другого массива путем удаления некоторых элементов или их
 отсутствия без изменения порядка остальных элементов.
-*/
 
-/*
 Input: nums = [4,5,2,1], queries = [3,10,21]
 Output: [2,3,4]
 Пояснение: На запросы мы отвечаем следующим образом:
@@ -16,9 +14,7 @@ Output: [2,3,4]
 подпоследовательности, поэтому ответ[1] = 3.
 - Сумма подпоследовательности [4,5,2,1] меньше или равна 21. Можно доказать, что 4 — это максимальный размер такой
 подпоследовательности, поэтому ответ[2] = 4. 4.
- */
 
-/*
 Input: nums = [2,3,4,5], queries = [1]
 Output: [0]
 Объяснение: Пустая подпоследовательность — единственная подпоследовательность, сумма которой меньше или равна 1,
@@ -31,18 +27,18 @@ Output: [0]
  * @return {number[]}
  */
 var answerQueries = function(nums, queries) {
-    nums.sort((a,b) => a - b);
+    nums.sort((a,b) => a - b); // [1, 2, 4, 5]
     const prefixSum = [nums[0]];
 
     for (let i = 1; i < nums.length; i++) {
-        prefixSum.push(prefixSum[i - 1] + nums[i]);
+        prefixSum.push(nums[i] + prefixSum[i - 1]);
     }
 
     const answer = [];
 
     for (let i = 0; i < queries.length; i++) {
         const target = queries[i];
-        let [left, right] = [0, prefixSum.length - 1];
+        let [left, right] = [0, prefixSum.length - 1]; // prefixSum: [1, 3, 7, 12]
 
         while(left <= right){
             let mid = Math.floor((left + right) / 2);
