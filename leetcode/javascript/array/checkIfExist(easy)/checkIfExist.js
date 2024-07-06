efa006991122b67101df04df13687a55c560a881
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-06 07:20:08 +0300

    1346. Check If N and Its Double Exist

--- leetcode/javascript/array/checkIfExist(easy)/checkIfExist.js
+++ leetcode/javascript/array/checkIfExist(easy)/checkIfExist.js
@@ -1,19 +1,13 @@
-// https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
-
-/*
+/* https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
 Учитывая массив целых чисел, проверьте, существуют ли два индекса i и j такие, что:
 i != j
 0 <= i, j < arr.length
 arr[i] == 2 * arr[j]
-*/
 
-/*
 Input: arr = [10,2,5,3]
 Output: true
 Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
-*/
 
-/*
 Input: arr = [3,1,7,11]
 Output: false
 Explanation: There is no i and j that satisfy the conditions.
