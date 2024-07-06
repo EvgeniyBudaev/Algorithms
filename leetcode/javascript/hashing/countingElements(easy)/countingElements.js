d9b7f470441e848bc12c75618de9f476b42bfdb8
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-06 07:47:50 +0300

    1426. Counting Elements

--- leetcode/javascript/hashing/countingElements(easy)/countingElements.js
+++ leetcode/javascript/hashing/countingElements(easy)/countingElements.js
@@ -1,34 +1,23 @@
 /*
 Учитывая целочисленный массив arr, подсчитайте, сколько элементов x существует так, что x + 1 также находится в arr.
-
 Если в arr есть дубликаты, посчитайте их отдельно.
- */
 
-/*
 Input: arr = [1,2,3]
 Output: 2
 Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
- */
 
-/*
 Input: arr = [1,1,3,3,5,5,7,7]
 Output: 0
 Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
- */
 
-/*
 Input: arr = [1,3,2,3,5,0]
 Output: 3
 Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
- */
 
-/*
 Input: arr = [1,1,2,2]
 Output: 2
 Explanation: Two 1s are counted cause 2 is in arr.
- */
 
-/*
 Input: arr = [1,1,2]
 Output: 2
 Explanation: Both 1s are counted because 2 is in the array.
