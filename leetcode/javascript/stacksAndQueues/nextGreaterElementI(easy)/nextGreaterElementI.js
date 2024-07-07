716921dfb6fd5d02e977ca6c34a025e151edbe17
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-07 14:35:38 +0300

    496. Next Greater Element I

--- leetcode/javascript/stacksAndQueues/nextGreaterElementI(easy)/nextGreaterElementI.js
+++ leetcode/javascript/stacksAndQueues/nextGreaterElementI(easy)/nextGreaterElementI.js
@@ -1,35 +1,31 @@
-/*
+/* https://leetcode.com/problems/next-greater-element-i/description/
 Следующий больший элемент некоторого элемента x в массиве — это первый больший элемент, находящийся справа от x в том же
 массиве.
 Вам даны два различных целочисленных массива с нулевым индексом nums1 и nums2, где nums1 — это подмножество nums2.
 Для каждого 0 <= i < nums1.length найдите индекс j такой, что nums1[i] == nums2[j] и определите следующий больший
 элемент nums2[j] в nums2. Если следующего большего элемента нет, то ответом на этот запрос будет -1.
 Верните массив ans длины nums1.length такой, что ans[i] является следующим большим элементом, как описано выше.
-*/
 
-/*
 Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
 Output: [-1,3,-1]
-Explanation: The next greater element for each value of nums1 is as follows:
-- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
-- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
-- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
- */
+Объяснение: Следующий больший элемент для каждого значения nums1 выглядит следующим образом:
+- 4 подчеркнуто в nums2 = [1,3,4,2]. Следующего большего элемента нет, поэтому ответ - -1.
+- 1 подчеркнута в nums2 = [1,3,4,2]. Следующий больший элемент — 3.
+- 2 подчеркнуто в nums2 = [1,3,4,2]. Следующего большего элемента нет, поэтому ответ - -1.
 
-/*
 Input: nums1 = [2,4], nums2 = [1,2,3,4]
 Output: [3,-1]
-Explanation: The next greater element for each value of nums1 is as follows:
-- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
-- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
- */
+Объяснение: Следующий больший элемент для каждого значения nums1 выглядит следующим образом:
+- 2 подчеркнуто в nums2 = [1,2,3,4]. Следующий больший элемент — 3.
+- 4 подчеркнуто в nums2 = [1,2,3,4]. Следующего большего элемента нет, поэтому ответ - -1.
+*/
 
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(nums1, nums2) {
+var nextGreaterElementI = function(nums1, nums2) {
     const stack = [];
     const map = {};
 
@@ -44,4 +40,4 @@ var nextGreaterElement = function(nums1, nums2) {
     return nums1.map(num => map[num]);
 };
 
-console.log(nextGreaterElement([4,1,2], [1,3,4,2])); // [-1,3,-1]
+console.log(nextGreaterElementI([4,1,2], [1,3,4,2])); // [-1,3,-1]
