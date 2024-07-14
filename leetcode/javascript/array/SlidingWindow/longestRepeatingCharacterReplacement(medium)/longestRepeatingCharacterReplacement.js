96d0fbc93acd573290e2947d2482c2bb6237878e
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-14 09:47:59 +0300

    424. Longest Repeating Character Replacement

---
+++ leetcode/javascript/array/SlidingWindow/longestRepeatingCharacterReplacement(medium)/longestRepeatingCharacterReplacement.js
@@ -0,0 +1,45 @@
+/* https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+Вам дана строка s и целое число k. Вы можете выбрать любой символ строки и заменить его на любой другой символ
+английского верхнего регистра. Эту операцию можно выполнить не более k раз.
+
+Возвращает длину самой длинной подстроки, содержащей ту же букву, которую вы можете получить после выполнения
+вышеуказанных операций.
+
+Input: s = "ABAB", k = 2
+Output: 4
+Пояснение: Замените две буквы «А» двумя буквами «B» или наоборот.
+
+Input: s = "AABABBA", k = 1
+Output: 4
+Пояснение: Замените букву «A» посередине на «B» и получите «AABBBBA».
+Подстрока «BBBB» имеет самую длинную повторяющуюся букву — 4.
+Могут существовать и другие способы получить этот ответ.
+*/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function(s, k) {
+    const map = {};
+    let longest = 0, maxFreq = 0;
+    let left = 0, right = 0;
+
+    while (right < s.length) {
+        let current = s[right];
+        map[current] = (map[current] || 0) + 1;
+        maxFreq = Math.max(maxFreq, map[current]);
+        if (right - left + 1 > maxFreq + k) {
+            map[s[left]]--;
+            left++;
+        }
+        longest = Math.max(longest, right - left + 1);
+        right++;
+    }
+
+    return longest;
+};
+
+console.log(characterReplacement("ABAB", 2)); // 4
