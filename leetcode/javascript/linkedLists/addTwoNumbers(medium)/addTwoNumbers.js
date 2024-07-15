1539ba043553a8d7580146571df137bf8812b02d
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-15 09:56:03 +0300

    2. Add Two Numbers

--- leetcode/javascript/linkedLists/addTwoNumbers(medium)/addTwoNumbers.js
+++ leetcode/javascript/linkedLists/addTwoNumbers(medium)/addTwoNumbers.js
@@ -72,4 +72,4 @@ var addTwoNumbers = function(l1, l2) {
 
 const l1 = createLinkedList([2,4,3]);
 const l2 = createLinkedList([5,6,4]);
-console.log(addTwoNumbers(l1, l2));
+console.log(addTwoNumbers(l1, l2)); // [7,0,8]
