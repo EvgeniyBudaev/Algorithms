1ad712762497ca6f26d0d0702cdb2741f75600bd
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-15 09:54:42 +0300

    2. Add Two Numbers

---
+++ leetcode/javascript/linkedLists/addTwoNumbers(medium)/addTwoNumbers.js
@@ -0,0 +1,75 @@
+/* https://leetcode.com/problems/add-two-numbers/description/
+
+Вам даны два непустых связанных списка, представляющих два неотрицательных целых числа. Цифры хранятся в обратном
+порядке, и каждый из их узлов содержит одну цифру. Сложите два числа и верните сумму в виде связанного списка.
+Вы можете предположить, что эти два числа не содержат ведущих нулей, кроме самого числа 0.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+*/
+
+class ListNode {
+    constructor(val, next) {
+        this.val = (val!== undefined? val : 0);
+        this.next = (next!== undefined? next : null);
+    }
+}
+// Функция для создания односвязного списка
+function createLinkedList(arr) {
+    let head = null;
+    let current = null;
+    arr.forEach(val => {
+        const newNode = new ListNode(val);
+        if (!head) {
+            head = newNode;
+            current = newNode;
+        } else {
+            current.next = newNode;
+            current = newNode;
+        }
+    });
+    return head;
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let dummy = new ListNode();
+    let res = dummy;
+    let total = 0, carry = 0;
+
+    while (l1 || l2 || carry) {
+        total = carry;
+
+        if (l1) {
+            total += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            total += l2.val;
+            l2 = l2.next;
+        }
+
+        let num = total % 10;
+        carry = Math.floor(total / 10);
+        dummy.next = new ListNode(num);
+        dummy = dummy.next;
+    }
+
+    return res.next;
+};
+
+const l1 = createLinkedList([2,4,3]);
+const l2 = createLinkedList([5,6,4]);
+console.log(addTwoNumbers(l1, l2));
