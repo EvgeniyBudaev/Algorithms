2e00c9d438b96517524c754c21090d1d838f72ce
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-21 13:11:01 +0300

    5. Longest Palindromic Substring

---
+++ leetcode/javascript/dynamicProgramming/longestPalindromicSubstring(medium)/longestPalindromicSubstring.js
@@ -0,0 +1,45 @@
+/* https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+Учитывая строку s, вернуть самую длинную палиндромный подстрока в s.
+
+Input: s = "babad"
+Output: "bab"
+Пояснение: «aba» также является допустимым ответом.
+
+Input: s = "cbbd"
+Output: "bb"
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    if (!s) return "";
+
+    function expandAroundCenter(s, left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const odd = expandAroundCenter(s, i, i);
+        const even = expandAroundCenter(s, i, i + 1);
+        const max_len = Math.max(odd, even);
+
+        if (max_len > end - start) {
+            start = i - Math.floor((max_len - 1) / 2);
+            end = i + Math.floor(max_len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
+};
+
+console.log(longestPalindrome("babad")); // "bab"
