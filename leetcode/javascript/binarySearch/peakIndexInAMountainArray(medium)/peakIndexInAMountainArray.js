24f3f2fe4dfa8556a0c88d0fd3a333ff52f82875
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-24 18:22:01 +0300

    852. Peak Index in a Mountain Array

---
+++ leetcode/javascript/binarySearch/peakIndexInAMountainArray(medium)/peakIndexInAMountainArray.js
@@ -0,0 +1,33 @@
+/* https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+Вам дан целочисленный горный массив длиной n, значения которого увеличиваются до пикового элемента, а затем уменьшаются.
+Верните индекс пикового элемента.
+Ваша задача — решить ее за время сложности O(log(n)).
+
+Input: arr = [0,1,0]
+Output: 1
+
+Input: arr = [0,2,1,0]
+Output: 1
+
+Input: arr = [0,10,5,2]
+Output: 1
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(arr) {
+    let left = 0, right = arr.length - 1;
+
+    while (left < right) {
+        const  mid = Math.floor(left + ((right - left) / 2));
+        if (arr[mid] < arr[mid + 1]) left = mid + 1;
+        else right = mid;
+    }
+    return left;
+};
+
+const arr = [0,1,0];
+console.log(peakIndexInMountainArray(arr)); // 1
