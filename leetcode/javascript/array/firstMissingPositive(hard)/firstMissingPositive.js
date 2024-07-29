25305adf3605f554843b921d27cf44e7d0d6d3e3
Evgeniy Budaev <budaev.e@gmail.com>
2024-07-29 07:44:30 +0300

    41. First Missing Positive

---
+++ leetcode/javascript/array/firstMissingPositive(hard)/firstMissingPositive.js
@@ -0,0 +1,34 @@
+/* https://leetcode.com/problems/first-missing-positive/description/
+
+Дан несортированный целочисленный массив nums. Возвращает наименьшее положительное целое число, которого нет в nums.
+Вы должны реализовать алгоритм, который работает за время O(n) и использует вспомогательное пространство O(1).
+
+Input: nums = [1,2,0]
+Output: 3
+Объяснение: Все числа в диапазоне [1,2] входят в массив
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Пояснение: 1 есть в массиве, но 2 отсутствует.
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Объяснение: Пропущено наименьшее положительное целое число 1.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+  const numSet = new Set(nums.filter(num => num > 0));
+  let i = 1;
+
+  while (numSet.has(i)) {
+    i++;
+  }
+
+  return i;
+};
+
+console.log(firstMissingPositive([1,2,0])); // 3
