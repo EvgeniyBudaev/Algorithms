72e17edbb29e4877657812c10169b2dbbcbf212e
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-03 08:47:26 +0300

    121. Best Time to Buy and Sell Stock

--- leetcode/javascript/array/bestTimeToBuyAndSellStock(easy)/bestTimeToBuyAndSellStock.js
+++ leetcode/javascript/array/bestTimeToBuyAndSellStock(easy)/bestTimeToBuyAndSellStock.js
@@ -14,7 +14,7 @@ Output: 5
 Input: prices = [7,6,4,3,1]
 Output: 0
 Объяснение: В этом случае транзакции не выполняются, а максимальная прибыль = 0.
- */
+*/
 
 /**
  * @param {number[]} prices
@@ -23,12 +23,10 @@ Output: 0
 var maxProfit = function(prices) {
   let buyPrice = prices[0];
   let profit = 0;
-
   for (let i = 1; i < prices.length; i++) {
-    if (buyPrice > prices[i]) buyPrice = prices[i];
+    if (prices[i] < buyPrice) buyPrice = prices[i];
     profit = Math.max(profit, prices[i] - buyPrice);
   }
-
   return profit;
 };
 
