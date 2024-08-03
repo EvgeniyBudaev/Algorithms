115f364866a0dab34a0e774c2fcc6e2b3b65f0d1
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-03 13:43:27 +0300

    1089. Duplicate Zeros

--- leetcode/javascript/array/duplicateZeros(easy)/duplicateZeros.js
+++ leetcode/javascript/array/duplicateZeros(easy)/duplicateZeros.js
@@ -1,19 +1,13 @@
-// https://leetcode.com/problems/duplicate-zeros/description/
+/* https://leetcode.com/problems/duplicate-zeros/description/
 
-/*
 Учитывая целочисленный массив фиксированной длины arr, дублируйте каждое вхождение нуля, сдвигая оставшиеся элементы
-вправо.
-Обратите внимание, что элементы, превышающие длину исходного массива, не записываются. Внесите вышеуказанные изменения
-во входной массив и ничего не возвращайте.
-*/
+вправо. Обратите внимание, что элементы, превышающие длину исходного массива, не записываются. Внесите вышеуказанные
+изменения во входной массив и ничего не возвращайте.
 
-/*
 Input: arr = [1,0,2,3,0,4,5,0]
 Output: [1,0,0,2,3,0,0,4]
 Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
-*/
 
-/*
 Input: arr = [1,2,3]
 Output: [1,2,3]
 Explanation: After calling your function, the input array is modified to: [1,2,3]
