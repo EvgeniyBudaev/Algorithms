444538d2e72ed31737b87fc3732ac38c7cd2977a
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-03 14:17:51 +0300

    287. Find the Duplicate Number

--- leetcode/javascript/array/findTheDuplicateNumber(medium)/findTheDuplicateNumber.js
+++ leetcode/javascript/array/findTheDuplicateNumber(medium)/findTheDuplicateNumber.js
@@ -24,4 +24,12 @@ var findDuplicate = function(nums) {
     }
 };
 
+var findDuplicate2 = function(nums) {
+    const set = new Set();
+    for (const num of nums) {
+        if (set.has(num)) return num;
+        else set.add(num);
+    }
+};
+
 console.log(findDuplicate([1,3,4,2,2])); // 2
