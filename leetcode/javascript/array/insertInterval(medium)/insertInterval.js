fe917e537b78d183c8dbd2ce4e793aaa02dfdca6
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-03 16:38:33 +0300

    57. Insert Interval

--- leetcode/javascript/array/insertInterval(medium)/insertInterval.js
+++ leetcode/javascript/array/insertInterval(medium)/insertInterval.js
@@ -7,7 +7,7 @@
 Вставьте newInterval в интервалы таким образом, чтобы интервалы по-прежнему сортировались в порядке возрастания по
 начальным значениям, а интервалы по-прежнему не имели перекрывающихся интервалов (при необходимости объединяйте
 перекрывающиеся интервалы).
-Интервалы возврата после вставки.
+Интервалы верните после вставки.
 Обратите внимание, что вам не нужно изменять интервалы на месте. Вы можете создать новый массив и вернуть его.
 
 Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
@@ -23,10 +23,37 @@ Output: [[1,2],[3,10],[12,16]]
  * @param {number[]} newInterval
  * @return {number[][]}
  */
+
 var insert = function(intervals, newInterval) {
+    let result = [];
+
+    for (let i = 0; i < intervals.length; i++) {
+        let currentInterval = intervals[i];
+
+        // Если текущий интервал заканчивается до начала нового, добавьте его к результату
+        if (currentInterval[1] < newInterval[0]) {
+            result.push(currentInterval);
+        }
+        // Если текущий интервал начинается после окончания нового интервала, добавляем новый интервал и обновляем его
+        // до текущего интервала
+        else if (currentInterval[0] > newInterval[1]) {
+            result.push(newInterval);
+            newInterval = currentInterval;
+        }
+        // Если интервалы перекрываются, объедините их
+        else {
+            newInterval[0] = Math.min(newInterval[0], currentInterval[0]);
+            newInterval[1] = Math.max(newInterval[1], currentInterval[1]);
+        }
+    }
+
+    result.push(newInterval);
+    return result;
+};
+
+var insert2 = function(intervals, newInterval) {
     let [start, end] = newInterval;
-    let left = [];
-    let right = [];
+    const left = [], right = [];
     for (const interval of intervals) {
         const [first, last] = interval;
         if (last < start) left.push(interval);
