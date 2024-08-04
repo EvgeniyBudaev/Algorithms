cd149bd3eb5b033e86aefbf60f65d252d262770c
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-04 10:45:18 +0300

    169. Majority Element

--- leetcode/javascript/array/majorityElement(easy)/majorityElement.js
+++ leetcode/javascript/array/majorityElement(easy)/majorityElement.js
@@ -19,16 +19,11 @@ Output: 3
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let count = 0;
-    let candidate = 0;
-
+    let count = 0, candidate = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (count === 0) {
-            candidate = nums[i];
-        }
+        if (count === 0) candidate = nums[i];
         count += candidate === nums[i] ? 1 : -1;
     }
-
     return candidate;
 };
 
