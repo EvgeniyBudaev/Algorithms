4cfa8e28a3ce2f4a65817bcd7f56400f97e9066b
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-05 11:50:39 +0300

    417. Pacific Atlantic Water Flow

--- leetcode/javascript/array/pacificAtlanticWaterFlow(medium)/pacificAtlanticWaterFlow.js
+++ leetcode/javascript/array/pacificAtlanticWaterFlow(medium)/pacificAtlanticWaterFlow.js
@@ -8,9 +8,10 @@ solution https://leetcode.com/problems/pacific-atlantic-water-flow/solutions/101
 представляет высоту над уровнем моря ячейки в координате (r, c).
 
 На острове выпадает много дождя, и дождевая вода может течь в соседние ячейки прямо на север, юг, восток и запад, если
-высота соседней ячейки меньше или равна высоте текущей ячейки. Вода может перетекать из любой клетки, прилегающей к океану, в океан.
+высота соседней ячейки меньше или равна высоте текущей ячейки. Вода может перетекать из любой клетки, прилегающей к
+океану, в океан.
 
-Возвращает двумерный список координат сетки, где result[i] = [ri, ci] означает, что дождевая вода может течь из ячейки
+Верните двумерный список координат сетки, где result[i] = [ri, ci] означает, что дождевая вода может течь из ячейки
 (ri, ci) как в Тихий, так и в Атлантический океаны.
 Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
 Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
@@ -38,29 +39,29 @@ Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
  */
 var pacificAtlantic = function(heights) {
     if (heights.length === 0) return [];
-    let numRows = heights.length;
-    let numCols = heights[0].length;
-
+    let rows = heights.length;
+    let cols = heights[0].length;
     let atlantic = [];
     let pacific = [];
-    for (let i = 0; i < numRows; i++) {
-        atlantic.push(new Array(numCols).fill(false));
-        pacific.push(new Array(numCols).fill(false));
+
+    for (let i = 0; i < rows; i++) {
+        atlantic.push(new Array(cols).fill(false));
+        pacific.push(new Array(cols).fill(false));
     }
 
-    for (let col = 0; col < heights[0].length; col++) {
+    for (let col = 0; col < cols; col++) {
         dfs(heights, 0, col, Number.MIN_SAFE_INTEGER, pacific);
-        dfs(heights, numRows - 1, col, Number.MIN_SAFE_INTEGER, atlantic);
+        dfs(heights, rows - 1, col, Number.MIN_SAFE_INTEGER, atlantic);
     }
 
-    for (let row = 0;row < heights.length; row++) {
+    for (let row = 0; row < rows; row++) {
         dfs(heights, row, 0, Number.MIN_SAFE_INTEGER, pacific);
-        dfs(heights, row, numCols - 1, Number.MIN_SAFE_INTEGER, atlantic);
+        dfs(heights, row, cols - 1, Number.MIN_SAFE_INTEGER, atlantic);
     }
 
     let res = [];
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j < numCols; j++){
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
             if (atlantic[i][j] && pacific[i][j]) res.push([i, j]);
         }
     }
@@ -70,22 +71,21 @@ var pacificAtlantic = function(heights) {
 
 
 const dfs = (matrix, i, j, prev, ocean) =>{
-    //checkOutOfBounds
-    if (i<0 ||
+    // проверить за пределами границ
+    if (i < 0 ||
         i > matrix.length -1 ||
         j < 0 ||
         j > matrix[i].length - 1
     ) return;
-
-
     if (matrix[i][j] < prev) return;
     if (ocean[i][j]) return;
+
     ocean[i][j] = true;
 
-    dfs(matrix, i+1, j, matrix[i][j], ocean);
-    dfs(matrix, i-1, j, matrix[i][j], ocean);
-    dfs(matrix, i, j+1, matrix[i][j], ocean);
-    dfs(matrix, i, j-1, matrix[i][j], ocean);
+    dfs(matrix, i + 1, j, matrix[i][j], ocean);
+    dfs(matrix, i - 1, j, matrix[i][j], ocean);
+    dfs(matrix, i, j + 1, matrix[i][j], ocean);
+    dfs(matrix, i, j - 1, matrix[i][j], ocean);
 }
 
 const heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]];
