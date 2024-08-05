a859759dbb7d8d1699bd7c8f0bbfc5295f0d3b70
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-05 10:08:24 +0300

    161. One Edit Distance

--- leetcode/javascript/array/oneEditDistance(medium)/oneEditDistance.js
+++ leetcode/javascript/array/oneEditDistance(medium)/oneEditDistance.js
@@ -1,7 +1,7 @@
-/* https://github.com/EvgeniyBudaev/doocs_leetcode/tree/main/solution/0100-0199/0161.One%20Edit%20Distance
+/* 161. One Edit Distance
+https://github.com/EvgeniyBudaev/doocs_leetcode/tree/main/solution/0100-0199/0161.One%20Edit%20Distance
 
-Учитывая две строки s и, вернитесь, t если их расстояние редактирования равно, иначе верните .1 true false
-Возможны три ситуации, в которых расстояние редактирования между строкой s и строкой равно 1:t
+Учитывая две строки s и t, определите, находятся ли они на расстоянии одного редактирования друг от друга.
 
 s Вставьте в него ровно один символ , чтобы получить t
 s Удалите из него ровно один символ, чтобы получить t
@@ -10,13 +10,16 @@ s Удалите из него ровно один символ, чтобы по
 Input: s = "ab", t = "acb"
 Output: true
 Объяснение: в строку s можно вставить 'c',  чтобы получить t.
+
+Input: s = "cab", t = "ad"
+Output: false
 */
 
 function isOneEditDistance(s, t) {
     const [m, n] = [s.length, t.length];
-    if (m < n) return isOneEditDistance(t, s);
+    if (m < n) return isOneEditDistance(t, s); // "acb", "ab"
     if (m - n > 1) return false;
-    for (let i = 0; i < n; ++i) {
+    for (let i = 0; i < n; i++) {
         if (s[i] !== t[i]) return s.slice(i + 1) === t.slice(i + (m === n ? 1 : 0));
     }
     return m === n + 1;
