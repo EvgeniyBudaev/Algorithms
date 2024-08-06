42ee5e3919e1813839b163f6c702757502cd4314
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-06 10:02:39 +0300

    1. Two Sum

--- leetcode/javascript/array/twoSum(easy)/twoSum.js
+++ leetcode/javascript/array/twoSum(easy)/twoSum.js
@@ -1,4 +1,5 @@
 /* https://leetcode.com/problems/two-sum/description/
+
 Учитывая массив целых чисел nums и целочисленную target, верните индексы двух чисел так, чтобы их сумма составляла
 target. Вы можете предположить, что каждый вход будет иметь ровно одно решение, и вы не можете использовать один и тот
 же элемент дважды. Вы можете вернуть ответ в любом порядке.
@@ -23,13 +24,23 @@ Output: [0,1]
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const mp = {};
-    for (let i = 0; i < nums.length; i++) {
-        const diff = target - nums[i];
-        if (diff in mp) return [i, mp[diff]];
-        mp[nums[i]] = i;
+    const map = new Map();
+    for (let i = 0; i < nums.length; ++i) {
+        const num = nums[i];
+        const diff = target - num;
+        if (map.has(diff)) return [map.get(diff), i];
+        map.set(num, i);
     }
 };
+
+// var twoSum = function(nums, target) {
+//     const mp = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         const diff = target - nums[i];
+//         if (diff in mp) return [i, mp[diff]];
+//         mp[nums[i]] = i;
+//     }
+// };
 
 //console.log(twoSum([2, 7, 11, 15], 9)); //  [0, 1]
 console.log(twoSum([3, 2, 4], 6)); // [1, 2]
