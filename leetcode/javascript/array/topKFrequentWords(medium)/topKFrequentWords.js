5b268642e34b44ace6b14c5cca6028b6adc48260
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-06 09:50:13 +0300

    692. Top K Frequent Words

--- leetcode/javascript/array/topKFrequentWords(medium)/topKFrequentWords.js
+++ leetcode/javascript/array/topKFrequentWords(medium)/topKFrequentWords.js
@@ -16,15 +16,14 @@ Output: ["i","love"]
  * @return {string[]}
  */
 var topKFrequent = function(words, k) {
-    const hashmap = new Map();
+    const map = new Map();
 
     for (let i = 0; i < words.length; i++) {
         const word = words[i];
-        if (!hashmap.has(word)) hashmap.set(word, 1);
-        else hashmap.set(word, hashmap.get(word) + 1);
+        map.set(word, (map.get(word) ?? 0) + 1);
     }
 
-    const arr = [...hashmap.entries()]
+    const arr = [...map.entries()]
         .sort((a, b) => b[1] !== a[1] ? b[1] - a[1] : a[0]
             .localeCompare(b[0]));
 
