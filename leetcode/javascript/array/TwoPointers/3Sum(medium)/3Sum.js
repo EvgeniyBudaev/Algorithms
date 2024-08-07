39701657fa1e0cd7abdc04a292a1c7460a0242b7
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-07 07:48:44 +0300

    15. 3Sum

--- leetcode/javascript/array/TwoPointers/3Sum(medium)/3Sum.js
+++ leetcode/javascript/array/TwoPointers/3Sum(medium)/3Sum.js
@@ -1,7 +1,7 @@
 /* https://leetcode.com/problems/3sum/description/
 
 Учитывая целочисленный массив nums, верните все тройки [nums[i], nums[j], nums[k]] такие,
-что i != j, i != k и j != k и nums[i] + числа[j] + числа[к] == 0.
+что i != j, i != k и j != k и nums[i] + nums[j] + nums[к] == 0.
 Обратите внимание, что набор решений не должен содержать повторяющихся троек.
 
 Input: nums = [-1,0,1,2,-1,-4]
@@ -28,23 +28,23 @@ Output: [[0,0,0]]
  */
 var threeSum = function(nums) {
     const res = [];
-    nums.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b); // [-4,-1,-1,0,1,2]
 
     for (let i = 0; i < nums.length; i++) {
-        const a = nums[i];
-        if (a > 0) break;
-        if (i > 0 && a === nums[i - 1]) continue;
+        const num = nums[i];
+        if (num > 0) break;
+        if (i > 0 && num === nums[i - 1]) continue;
 
         let left = i + 1;
         let right = nums.length - 1;
         while (left < right) {
-            const threeSum = a + nums[left] + nums[right];
+            const threeSum = num + nums[left] + nums[right];
             if (threeSum > 0) {
                 right--;
             } else if (threeSum < 0) {
                 left++;
             } else {
-                res.push([a, nums[left], nums[right]]);
+                res.push([num, nums[left], nums[right]]);
                 left++;
                 right--;
                 while (nums[left] === nums[left - 1] && left < right) {
