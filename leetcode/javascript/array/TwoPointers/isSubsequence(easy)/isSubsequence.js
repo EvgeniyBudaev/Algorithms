084aa333249a83b88b83e309962c5058c7829b6a
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-08 07:57:43 +0300

    Is Subsequence

--- leetcode/javascript/array/TwoPointers/isSubsequence(easy)/isSubsequence.js
+++ leetcode/javascript/array/TwoPointers/isSubsequence(easy)/isSubsequence.js
@@ -13,15 +13,10 @@
  */
 var isSubsequence = function(s, t) {
     let i = 0, j = 0;
-
     while (i < s.length && j < t.length) {
-        if (s[i] === t[j]) {
-            i++;
-        }
-
+        if (s[i] === t[j]) i++;
         j++;
     }
-
     return i === s.length;
 };
 
