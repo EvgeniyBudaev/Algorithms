91306f51abdb649e1700cfba1cc072303d106444
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-10 15:41:02 +0300

    1004. Max Consecutive Ones III

--- leetcode/javascript/array/SlidingWindow/maxConsecutiveOnesIII(medium)/maxConsecutiveOnesIII.js
+++ leetcode/javascript/array/SlidingWindow/maxConsecutiveOnesIII(medium)/maxConsecutiveOnesIII.js
@@ -1,4 +1,5 @@
 /* https://leetcode.com/problems/max-consecutive-ones-iii/description/
+
 Учитывая двоичный массив nums и целое число k, верните максимальное количество последовательных единиц в массиве,
 если вы можете перевернуть не более k нулей.
 
@@ -10,8 +11,7 @@ Explanation: [1,1,1,0,0,1,1,1,1,1,1]
 Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
 Output: 10
 Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
- */
-
+*/
 
 /**
  * @param {number[]} nums
@@ -19,20 +19,17 @@ Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    // curr — текущее количество нулей в окне
-    let left = 0, curr = 0, ans = 0;
+    let left = 0, zeroCount = 0, ans = 0;
+
     for (let right = 0; right < nums.length; right++) {
-        if (nums[right] === 0) {
-            curr++;
-        }
-        while (curr > k) {
-            if (nums[left] === 0) {
-                curr--;
-            }
+        if (nums[right] === 0) zeroCount++;
+        while (zeroCount > k) {
+            if (nums[left] === 0) zeroCount--;
             left++;
         }
         ans = Math.max(ans, right - left + 1);
     }
+
     return ans;
 };
 
