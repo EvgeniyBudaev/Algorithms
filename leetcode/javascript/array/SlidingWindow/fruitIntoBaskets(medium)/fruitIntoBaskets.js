33121fab01cbf8c9b92d4e0879877b18e6e211a0
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-10 10:48:22 +0300

    904. Fruit Into Baskets

--- leetcode/javascript/array/SlidingWindow/fruitIntoBaskets(medium)/fruitIntoBaskets.js
+++ leetcode/javascript/array/SlidingWindow/fruitIntoBaskets(medium)/fruitIntoBaskets.js
@@ -33,21 +33,21 @@ Output: 4
  * @return {number}
  */
 var totalFruit = function(fruits) {
-    const hashMap = new Map();
-    let left = 0, maxLen = 0;
+    const map = new Map();
+    let left = 0, max = 0;
     for (let right = 0; right < fruits.length; right++) {
         const rightFruit = fruits[right];
-        hashMap.set(rightFruit, hashMap.get(rightFruit) + 1 || 0);
-        while (hashMap.size > 2) {
+        map.set(rightFruit, map.get(rightFruit) + 1 || 0);
+        while (map.size > 2) {
             const leftFruit = fruits[left];
-            if (hashMap.get(leftFruit) === 0) hashMap.delete(leftFruit);
-            else hashMap.set(leftFruit, hashMap.get(leftFruit) - 1);
+            if (map.get(leftFruit) === 0) map.delete(leftFruit);
+            else map.set(leftFruit, map.get(leftFruit) - 1);
             left++;
         }
-        maxLen = Math.max(maxLen, right - left + 1);
+        max = Math.max(max, right - left + 1);
     }
-    return maxLen;
+    return max;
 };
 
 const fruits = [1,2,1];
-console.log(totalFruit(fruits));
+console.log(totalFruit(fruits)); // 3
