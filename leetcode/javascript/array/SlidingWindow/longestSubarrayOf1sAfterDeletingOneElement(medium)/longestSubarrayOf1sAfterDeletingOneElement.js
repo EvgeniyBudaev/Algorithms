d23a2ef88927522e616c699d1832c287b85a4281
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-10 13:04:28 +0300

    1493. Longest Subarray of 1's After Deleting One Element

--- leetcode/javascript/array/SlidingWindow/longestSubarrayOf1sAfterDeletingOneElement(medium)/longestSubarrayOf1sAfterDeletingOneElement.js
+++ leetcode/javascript/array/SlidingWindow/longestSubarrayOf1sAfterDeletingOneElement(medium)/longestSubarrayOf1sAfterDeletingOneElement.js
@@ -22,7 +22,7 @@ Output: 2
  * @return {number}
  */
 var longestSubarray = function(nums) {
-    let left = 0, result = 0, zeroCount = 0;
+    let left = 0, ans = 0, zeroCount = 0;
 
     for (let right = 0; right < nums.length; right++) {
         if (nums[right] === 0) zeroCount++;
@@ -30,10 +30,10 @@ var longestSubarray = function(nums) {
             if (nums[left] === 0) zeroCount--;
             left++;
         }
-        if (zeroCount <= 1) result = Math.max(result, right - left);
+        ans = Math.max(ans, right - left);
     }
 
-    return result;
+    return ans;
 };
 
 console.log(longestSubarray([1,1,0,1])); // 3
