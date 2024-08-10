71ccbc70f595d69e2341511d4a034c0e69f436fc
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-10 20:44:51 +0300

    example

--- example/example.js
+++ example/example.js
@@ -1,19 +1,4 @@
-const example = (nums, k) => {
-   let left = 0, curr = 1, ans = 0;
-
-   for (let right = 0; right < nums.length; right++) {
-       curr *= nums[right];
-       console.log(curr);
-
-       while (curr >= k) {
-           curr /= nums[left];
-           left++;
-       }
-
-       ans += right - left + 1;
-   }
-
-   return ans;
+const example = () => {
 }
 
-console.log(example([10, 5, 2, 6], 100)); // 5
+console.log(example());
