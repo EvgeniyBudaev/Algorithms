07572e02cc7249e0be6032250aa683308d935092
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-11 09:19:33 +0300

    76. Minimum Window Substring

--- leetcode/javascript/array/SlidingWindow/minimumWindowSubstring(hard)/minimumWindowSubstring.js
+++ leetcode/javascript/array/SlidingWindow/minimumWindowSubstring(hard)/minimumWindowSubstring.js
@@ -1,9 +1,8 @@
 /* https://leetcode.com/problems/minimum-window-substring/description/
 solution https://leetcode.com/problems/minimum-window-substring/solutions/4673781/easy-sliding-window-solution-explained/
 
-Учитывая две строки s и t длиной m и n соответственно, верните минимальное окно подстрока из s так, что каждый символ из
-t (включая дубликаты) включен в окно. Если такой подстроки нет, верните пустую строку «».
-Тестовые примеры будут сгенерированы таким образом, чтобы ответ был уникальным.
+Учитывая две строки s и t длиной m и n соответственно, верните минимальную подстроку окна s, такую, что каждый символ
+в t (включая дубликаты) включен в окно. Если такой подстроки нет, верните пустую строку «».
 
 Input: s = "ADOBECODEBANC", t = "ABC"
 Output: "BANC"
@@ -27,27 +26,28 @@ Output: ""
  */
 var minWindow = function(s, t) {
     if (s.length < t.length) return "";
-    let cnt = {}, len = t.length, res = [], min = Infinity;
+    let map = {}, len = t.length, ans = [], min = Infinity;
+
     for (let char of t) {
-        cnt[char] = (cnt[char] || 0) + 1;
+        map[char] = (map[char] || 0) + 1;
     }
 
-    for (let r = 0, l = 0; r < s.length; r++) {
-        let current = s[r];
-        if (cnt[current] > 0) len--;
-        cnt[current]--;
+    for (let right = 0, left = 0; right < s.length; right++) {
+        const current = s[right];
+        if (map[current] > 0) len--;
+        map[current]--;
         while (!len) {
-            if (r - l < min) {
-                min = r - l;
-                res = [l, r];
+            if (right - left < min) {
+                min = right - left;
+                ans = [left, right];
             }
-            if (cnt[s[l]] >= 0) len++;
-            cnt[s[l]]++;
-            l++;
+            if (map[s[left]] >= 0) len++;
+            map[s[left]]++;
+            left++;
         }
     }
 
-    return s.slice(res[0], res[1] + 1);
+    return s.slice(ans[0], ans[1] + 1);
 };
 
 console.log(minWindow("ADOBECODEBANC", "ABC")); // "BANC"
