e38d56e5bf8f1ad02d6954e55ef7f9acf4cf5ce8
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-11 07:31:19 +0300

    Find Length Subarray

--- leetcode/javascript/array/SlidingWindow/findLengthSubarray(easy)/findLengthSubarray.js
+++ leetcode/javascript/array/SlidingWindow/findLengthSubarray(easy)/findLengthSubarray.js
@@ -1,12 +1,10 @@
 /*
 Дан массив целых положительных чисел nums и целое число k. Найдите длину самого длинного подмассива, сумма которого
 меньше или равна k.
- */
 
-/*
 Input: nums = [3, 1, 2, 7, 4, 2, 1, 1, 5], k = 8
 Output: 4
- */
+*/
 
 /**
  * @param {number[]} nums
@@ -14,12 +12,12 @@ Output: 4
  * @return {number}
  */
 var findLengthSubarray = function(nums, k) {
-    let left = 0, curr = 0, ans = 0; // curr — текущая сумма окна
+    let left = 0, sum = 0, ans = 0;
 
     for (let right = 0; right < nums.length; right++) {
-        curr += nums[right];
-        while (curr > k) {
-            curr -= nums[left];
+        sum += nums[right];
+        while (sum > k) {
+            sum -= nums[left];
             left++;
         }
         ans = Math.max(ans, right - left + 1);
