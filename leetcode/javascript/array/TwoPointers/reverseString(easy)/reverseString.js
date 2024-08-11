e3001ba643ce3b12a9d456511d4e2bc8d8f594aa
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-11 12:12:17 +0300

    344. Reverse String

--- leetcode/javascript/array/TwoPointers/reverseString(easy)/reverseString.js
+++ leetcode/javascript/array/TwoPointers/reverseString(easy)/reverseString.js
@@ -17,9 +17,10 @@ var reverseString = function(s) {
     let left = 0, right = s.length - 1;
 
     while (left < right) {
-        let temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
+        // let temp = s[left];
+        // s[left] = s[right];
+        // s[right] = temp;
+        [s[left], s[right]] = [s[right], s[left]];
         left++;
         right--;
     }
