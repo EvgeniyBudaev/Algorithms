5573e223be4673ee77bcbc48ad6ad95a38d30e84
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-11 17:47:56 +0300

    485. Max Consecutive Ones

--- leetcode/javascript/array/SlidingWindow/maxConsecutiveOnes(easy)/maxConsecutiveOnes.js
+++ leetcode/javascript/array/SlidingWindow/maxConsecutiveOnes(easy)/maxConsecutiveOnes.js
@@ -15,7 +15,23 @@ Output: 2
  * @param {number[]} nums
  * @return {number}
  */
+
 const findMaxConsecutiveOnes = function(nums) {
+    let left = 0, zeroCount = 0, ans = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) zeroCount++;
+        while (zeroCount > 0) {
+            if (nums[left] === 0) zeroCount--;
+            left++;
+        }
+        ans = Math.max(ans, right - left + 1);
+    }
+
+    return ans;
+};
+
+const findMaxConsecutiveOnes2 = function(nums) {
     let count = 0, max = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 1) {
