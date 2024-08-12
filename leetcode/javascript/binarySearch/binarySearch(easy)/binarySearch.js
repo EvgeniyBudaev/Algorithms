94fb361395a360623cb361ac1b4d1ee126fe626a
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-12 07:00:02 +0300

    704. Binary Search

--- leetcode/javascript/binarySearch/binarySearch(easy)/binarySearch.js
+++ leetcode/javascript/binarySearch/binarySearch(easy)/binarySearch.js
@@ -19,19 +19,15 @@ Explanation: 2 does not exist in nums so return -1
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length - 1;
+    let low = 0, high = nums.length - 1;
 
     while (low <= high) {
         const mid = Math.floor((low + high) / 2);
         const guess = nums[mid];
 
         if (guess === target) return mid;
-        if (guess > target) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
-        }
+        if (guess > target) high = mid - 1;
+        else low = mid + 1;
     }
 
     return -1;
