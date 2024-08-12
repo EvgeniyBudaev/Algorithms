eceda13c53410972cbd30e4861ebbfbbacfb3c81
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-12 07:49:49 +0300

    162. Find Peak Element

--- leetcode/javascript/binarySearch/findPeakElement(medium)/findPeakElement.js
+++ leetcode/javascript/binarySearch/findPeakElement(medium)/findPeakElement.js
@@ -23,10 +23,10 @@ Output: 5
  */
 var findPeakElement = function(nums) {
     let l = 0, r = nums.length - 1;
-    while (l < r) {
-        const mid = Math.floor((l + r) / 2);
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2); // 1 -> 2
         if (nums[mid + 1] > nums[mid]) l = mid + 1;
-        else r = mid;
+        else r = mid - 1;
     }
     return l;
 };
