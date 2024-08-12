6fc6aa55b202d75b093a54bb1b1e6b61fbcc24ce
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-12 15:59:22 +0300

    240. Search a 2D Matrix II

--- leetcode/javascript/binarySearch/searchA2DMatrixII(medium)/searchA2DMatrixII.js
+++ leetcode/javascript/binarySearch/searchA2DMatrixII(medium)/searchA2DMatrixII.js
@@ -1,9 +1,9 @@
 /* https://leetcode.com/problems/search-a-2d-matrix-ii/description/
 
-Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the
-following properties:
-Integers in each row are sorted in ascending from left to right.
-Integers in each column are sorted in ascending from top to bottom.
+Напишите эффективный алгоритм, который ищет целевое значение в целочисленной матрице размером m x n. Эта матрица имеет
+следующие свойства:
+Целые числа в каждой строке сортируются по возрастанию слева направо.
+Целые числа в каждом столбце сортируются по возрастанию сверху вниз.
 
 Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
 Output: true
@@ -18,10 +18,10 @@ Output: false
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let n = matrix.length;
-    for (let i=0; i < n; i++) {
-        let row = matrix.pop();
-        if(row.includes(target)) return true;
+   const n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        const row = matrix.pop();
+        if (row.includes(target)) return true;
     }
     return false;
 };
