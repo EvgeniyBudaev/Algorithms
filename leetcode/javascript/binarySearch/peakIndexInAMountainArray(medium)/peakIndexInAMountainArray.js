c86e3b1ed0c60635aff54638774ef318b58ec905
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-12 11:24:08 +0300

    852. Peak Index in a Mountain Array

--- leetcode/javascript/binarySearch/peakIndexInAMountainArray(medium)/peakIndexInAMountainArray.js
+++ leetcode/javascript/binarySearch/peakIndexInAMountainArray(medium)/peakIndexInAMountainArray.js
@@ -21,13 +21,13 @@ Output: 1
 var peakIndexInMountainArray = function(arr) {
     let left = 0, right = arr.length - 1;
 
-    while (left < right) {
-        const  mid = Math.floor(left + ((right - left) / 2));
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
         if (arr[mid] < arr[mid + 1]) left = mid + 1;
-        else right = mid;
+        else right = mid - 1;
     }
+
     return left;
 };
 
-const arr = [0,1,0];
-console.log(peakIndexInMountainArray(arr)); // 1
+console.log(peakIndexInMountainArray([0,1,0])); // 1
