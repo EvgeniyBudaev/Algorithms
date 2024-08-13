7e9a1cab976b6e2cdb4b10a4a679309ae5ce65b7
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-13 11:11:25 +0300

    1323. Maximum 69 Number

--- leetcode/javascript/greedy/maximum69Number(easy)/maximum69Number.js
+++ leetcode/javascript/greedy/maximum69Number(easy)/maximum69Number.js
@@ -1,10 +1,9 @@
-/*
+/* 1323. Maximum 69 Number
+
 Вам дано целое положительное число, состоящее только из цифр 6 и 9.
 Верните максимальное число, которое вы можете получить, изменив не более одной цифры
 (6 становится 9, а 9 становится 6).
-*/
 
-/*
 Input: num = 9669
 Output: 9969
 Explanation:
@@ -13,26 +12,22 @@ Changing the second digit results in 9969.
 Changing the third digit results in 9699.
 Changing the fourth digit results in 9666.
 The maximum number is 9969.
- */
 
-/*
 Input: num = 9996
 Output: 9999
 Explanation: Changing the last digit 6 to 9 results in the maximum number.
- */
 
-/*
 Input: num = 9999
 Output: 9999
 Explanation: It is better not to apply any change.
- */
+*/
 
 /**
  * @param {number} num
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    let digit = num.toString().split('');
+    const digit = num.toString().split('');
 
     for (let i = 0; i < digit.length; i++) {
         if (digit[i] === '6') {
