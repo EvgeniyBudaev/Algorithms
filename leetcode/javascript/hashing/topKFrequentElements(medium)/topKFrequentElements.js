1ba10b75e01c0fd119f8fb61714c961c041f73a1
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-13 10:28:39 +0300

    347. Top K Frequent Elements

--- leetcode/javascript/hashing/topKFrequentElements(medium)/topKFrequentElements.js
+++ leetcode/javascript/hashing/topKFrequentElements(medium)/topKFrequentElements.js
@@ -16,12 +16,13 @@ Output: [1]
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-    let map = new Map();
+    const map = new Map();
 
     for (let num of nums) {
         map.set(num, (map.get(num) ?? 0) + 1);
     }
 
+    // sortedByValue: [[1,3], [2,2], [3,1]]
     const sortedByValue = [...map.entries()].sort((a, b) => b[1] - a[1]);
     return sortedByValue.slice(0, k).map(entry => entry[0]);
 };
