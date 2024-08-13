657701734c4936c6fa04707e1e5cf13d8e68e952
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-13 10:31:23 +0300

    242. Valid Anagram

--- leetcode/javascript/hashing/validAnagram(easy)/validAnagram.js
+++ leetcode/javascript/hashing/validAnagram(easy)/validAnagram.js
@@ -18,11 +18,10 @@ Output: false
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false;
-    let map = new Map();
+    const map = new Map();
 
     for (let char of s) {
-        if (map.has(char)) map.set(char, map.get(char) + 1);
-        else map.set(char, 1);
+        map.set(char, (map.get(char) ?? 0) + 1);
     }
 
     for (let char of t) {
