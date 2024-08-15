d232925c470941f3c84bee1ac4451ad0ad81648b
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-15 20:02:18 +0300

    216. Combination Sum III

--- leetcode/javascript/backtracking/combinationSumIII(medium)/combinationSumIII.js
+++ leetcode/javascript/backtracking/combinationSumIII(medium)/combinationSumIII.js
@@ -1,22 +1,17 @@
-// https://leetcode.com/problems/combination-sum-iii/description/
+/* https://leetcode.com/problems/combination-sum-iii/description/
 
-/*
 Найдите все допустимые комбинации k чисел, сумма которых равна n, такие, что выполняются следующие условия:
 Используются только цифры от 1 до 9.
 Каждое число используется не более одного раза.
 Возвращает список всех возможных допустимых комбинаций. Список не должен содержать одну и ту же комбинацию дважды,
 комбинации могут возвращаться в любом порядке.
-*/
 
-/*
 Input: k = 3, n = 7
 Output: [[1,2,4]]
 Объяснение:
 1 + 2 + 4 = 7
 Других допустимых комбинаций нет
- */
 
-/*
 Input: k = 3, n = 9
 Output: [[1,2,6],[1,3,5],[2,3,4]]
 Объяснение:
@@ -24,7 +19,7 @@ Output: [[1,2,6],[1,3,5],[2,3,4]]
 1 + 3 + 5 = 9
 2 + 3 + 4 = 9
 Других допустимых комбинаций нет.
- */
+*/
 
 /**
  * @param {number} k
@@ -32,9 +27,9 @@ Output: [[1,2,6],[1,3,5],[2,3,4]]
  * @return {number[][]}
  */
 var combinationSum3 = function(k, n) {
-    let res = [];
+    const res = [];
 
-    let backtracking = (currentDigit, sum, elements) => {
+    const backtracking = (currentDigit, sum, elements) => {
         if (currentDigit > 9) return;
         if (sum > n) return;
         if (elements.length > k) return;
@@ -49,8 +44,7 @@ var combinationSum3 = function(k, n) {
         }
     }
 
-    backtracking(0, 0, [])
-
+    backtracking(0, 0, []);
     return res;
 };
 
