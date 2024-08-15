32c74e09755d8e578fa74e07906aa8742ca401c0
Evgeniy Budaev <budaev.e@gmail.com>
2024-08-15 20:27:12 +0300

    22. Generate Parentheses

--- leetcode/javascript/backtracking/generateParentheses(medium)/generateParentheses.js
+++ leetcode/javascript/backtracking/generateParentheses(medium)/generateParentheses.js
@@ -1,38 +1,30 @@
-// https://leetcode.com/problems/generate-parentheses/description/
+/* https://leetcode.com/problems/generate-parentheses/description/
 
-/*
 Учитывая n пар круглых скобок, напишите функцию, которая генерирует все комбинации правильных круглых скобок.
-*/
 
-/*
 Input: n = 3
 Output: ["((()))","(()())","(())()","()(())","()()()"]
- */
 
-/*
 Input: n = 1
 Output: ["()"]
- */
+*/
 
 /**
  * @param {number} n
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
+    const result = [];
+
     function backtrack(s = '', left = 0, right = 0) {
         if (s.length === 2 * n) {
             result.push(s);
             return;
         }
-        if (left < n) {
-            backtrack(s + '(', left + 1, right);
-        }
-        if (right < left) {
-            backtrack(s + ')', left, right + 1);
-        }
+        if (left < n) backtrack(s + '(', left + 1, right);
+        if (right < left) backtrack(s + ')', left, right + 1);
     }
 
-    let result = [];
     backtrack();
     return result;
 };
