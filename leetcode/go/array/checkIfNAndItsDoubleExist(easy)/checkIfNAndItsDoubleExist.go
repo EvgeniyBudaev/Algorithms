57aaa0b6a0c823fc53adb8a7330617ffe5b981c7
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-07 15:47:19 +0300

    feat: 1346. Check If N and Its Double Exist

---
+++ leetcode/go/array/checkIfNAndItsDoubleExist(easy)/checkIfNAndItsDoubleExist.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/check-if-n-and-its-double-exist/description/
+
+Учитывая массив целых чисел, проверьте, существуют ли два индекса i и j такие, что:
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+
+Input: arr = [10,2,5,3]
+Output: true
+Explanation: For i = 0 and j = 2, arr[i] == 10 == 2 * 5 == 2 * arr[j]
+
+Input: arr = [3,1,7,11]
+Output: false
+Пояснение: Не существует i и j, удовлетворяющих этим условиям.
+*/
+
+func main() {
+	arr := []int{10, 2, 5, 3}
+	r := checkIfExist(arr)
+	fmt.Println(r)
+}
+
+func checkIfExist(arr []int) bool {
+	left := 0
+	right := 1
+
+	for left < len(arr)-1 {
+		if arr[left] == arr[right]*2 || arr[right] == arr[left]*2 {
+			return true
+		} else if right == len(arr)-1 {
+			left++
+			right = left + 1
+		} else {
+			right++
+		}
+	}
+
+	return false
+}
