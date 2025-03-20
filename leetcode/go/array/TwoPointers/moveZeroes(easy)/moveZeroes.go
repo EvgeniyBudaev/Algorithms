0cfdde5a736a5d24487134469c61f5b123ca4f92
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-20 07:47:55 +0300

    feat: 283. Move Zeroes

---
+++ leetcode/go/array/TwoPointers/moveZeroes(easy)/moveZeroes.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/move-zeroes/description/
+
+Учитывая целочисленный массив nums, переместите все 0 в его конец, сохраняя относительный порядок ненулевых элементов.
+Обратите внимание, что вы должны сделать это на месте, не копируя массив.
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Input: nums = [0]
+Output: [0]
+*/
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+}
+
+func moveZeroes(nums []int) {
+	left := 0
+	for right := 0; right < len(nums); right++ {
+		if nums[right] != 0 {
+			nums[right], nums[left] = nums[left], nums[right]
+			left++
+		}
+	}
+	fmt.Println(nums)
+}
