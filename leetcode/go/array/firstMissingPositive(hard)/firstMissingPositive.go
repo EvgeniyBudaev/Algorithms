5156cee1a095921152e246d294cf2df72c5d9225
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-21 14:38:40 +0300

    feat: 41. First Missing Positive

---
+++ leetcode/go/array/firstMissingPositive(hard)/firstMissingPositive.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/first-missing-positive/description/
+
+Дан несортированный целочисленный массив nums. Верните наименьшее положительное целое число, которого нет в nums.
+Вы должны реализовать алгоритм, который работает за время O(n) и использует вспомогательное пространство O(1).
+
+Input: nums = [1,2,0]
+Output: 3
+Объяснение: Все числа в диапазоне [1,2] входят в массив
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Пояснение: 1 есть в массиве, но 2 отсутствует.
+
+Input: nums = [7,8,9,11,12]
+Output: 1
+Объяснение: Пропущено наименьшее положительное целое число 1.
+*/
+
+func main() {
+	nums := []int{1, 2, 0}
+	r := firstMissingPositive(nums)
+	fmt.Println(r)
+}
+
+func firstMissingPositive(nums []int) int {
+	set := make(map[int]bool)
+	for _, num := range nums {
+		if num > 0 {
+			set[num] = true
+		}
+	}
+	i := 1
+	for set[i] {
+		i++
+	}
+	return i
+}
