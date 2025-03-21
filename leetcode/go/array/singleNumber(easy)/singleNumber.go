1dc94cd2a185b4e9dd52a9e325c77eda45284deb
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-21 19:08:32 +0300

    feat: 136. Single Number

---
+++ leetcode/go/array/singleNumber(easy)/singleNumber.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/* https://leetcode.com/problems/single-number/description/
+
+Учитывая непустой массив целых чисел nums, каждый элемент появляется дважды, кроме одного. Найдите этого единственного.
+Вы должны реализовать решение с линейной сложностью времени выполнения и использовать только постоянное дополнительное
+пространство.
+
+Input: nums = [2,2,1]
+Output: 1
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Input: nums = [1]
+Output: 1
+*/
+
+func main() {
+	nums := []int{2, 2, 1}
+	r := singleNumber(nums)
+	fmt.Println(r)
+}
+
+func singleNumber(nums []int) int {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i += 2 {
+		if nums[i] != nums[i+1] {
+			return nums[i]
+		}
+	}
+	return nums[len(nums)-1]
+}
