f6531067127b56cf843d71510169865755ffdebb
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-21 19:18:38 +0300

    feat: 560. Subarray Sum Equals K

---
+++ leetcode/go/array/subarraySumEqualsK(medium)/subarraySumEqualsK.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+Учитывая массив целых чисел nums и целое число k, верните общее количество подмассивов, сумма которых равна k.
+Подмассив — это непрерывная непустая последовательность элементов массива.
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+*/
+
+func main() {
+	nums := []int{1, 1, 1}
+	k := 2
+	r := subarraySum(nums, k)
+	fmt.Println(r)
+}
+
+func subarraySum(nums []int, k int) int {
+	// Создаем map для хранения сумм и их количества
+	sumMap := make(map[int]int)
+	sumMap[0] = 1 // Инициализируем с суммой 0, встречающейся 1 раз
+
+	sum := 0
+	count := 0
+
+	// Проходим по массиву
+	for _, num := range nums {
+		sum += num
+		// Если (sum - k) существует в map, увеличиваем count
+		if val, exists := sumMap[sum-k]; exists {
+			count += val
+		}
+		// Обновляем map с текущей суммой
+		sumMap[sum]++
+	}
+
+	return count
+}
