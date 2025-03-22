f910949251c66dd04460eec29ec44237e247986e
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-22 11:15:45 +0300

    feat: Find Length Subarray

---
+++ leetcode/go/array/SlidingWindow/findLengthSubarray(easy)/findLengthSubarray.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+/*
+Дан массив целых положительных чисел nums и целое число k. Найдите длину самого длинного подмассива, сумма которого
+меньше или равна k.
+
+Input: nums = [3, 1, 2, 7, 4, 2, 1, 1, 5], k = 8
+Output: 4
+*/
+
+func main() {
+	nums := []int{3, 1, 2, 7, 4, 2, 1, 1, 5}
+	fmt.Println(findLengthSubarray(nums, 8))
+}
+
+func findLengthSubarray(nums []int, k int) int {
+	left, sum, ans := 0, 0, 0
+
+	for right := 0; right < len(nums); right++ {
+		sum += nums[right]
+
+		// Если сумма превышает k, сдвигаем left
+		for sum > k {
+			sum -= nums[left]
+			left++
+		}
+
+		// Обновляем максимальную длину подмассива
+		currentLength := right - left + 1
+		if currentLength > ans {
+			ans = currentLength
+		}
+	}
+
+	return ans
+}
