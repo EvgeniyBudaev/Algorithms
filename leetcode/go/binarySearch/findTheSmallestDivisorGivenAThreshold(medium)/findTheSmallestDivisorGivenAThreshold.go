00a64eeee2e825b55d359708f4cd2df707b4b253
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-30 16:06:22 +0300

    feat: 1283. Find the Smallest Divisor Given a Threshold

---
+++ leetcode/go/binarySearch/findTheSmallestDivisorGivenAThreshold(medium)/findTheSmallestDivisorGivenAThreshold.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/* https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/description/
+
+Учитывая массив целых чисел nums и целочисленный threshold, мы выберем положительный целочисленный divisor, разделим на
+него весь массив и просуммируем результат деления. Найдите наименьший divisor, чтобы упомянутый выше результат был
+меньше или равен threshold.
+Каждый результат деления округляется до ближайшего целого числа, большего или равного этому элементу.
+(Например: 7/3 = 3 и 10/2 = 5).
+Тестовые случаи генерируются для того, чтобы был ответ.
+
+Input: nums = [1,2,5,9], threshold = 6
+Output: 5
+Пояснение: Мы можем получить сумму 17 (1+2+5+9), если делитель равен 1.
+Если делитель равен 4, мы можем получить сумму 7 (1+1+2+3), а если делитель равен 5, сумма будет 5 (1+1+1+2).
+
+Input: nums = [44,22,33,11,1], threshold = 5
+Output: 44
+*/
+
+func main() {
+	nums := []int{1, 2, 5, 9}
+	fmt.Println(smallestDivisor(nums, 6)) // 5
+}
+
+func smallestDivisor(nums []int, threshold int) int {
+	left := 1
+	right := maxInSlice(nums) // Находим максимальное значение в массиве
+
+	for left <= right {
+		mid := (left + right) / 2
+		if sumDivisions(nums, mid) <= threshold {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return left
+}
+
+// Вспомогательная функция для вычисления суммы округленных вверх частных
+func sumDivisions(nums []int, divisor int) int {
+	total := 0
+	for _, num := range nums {
+		total += int(math.Ceil(float64(num) / float64(divisor)))
+	}
+	return total
+}
+
+// Вспомогательная функция для нахождения максимального значения в срезе
+func maxInSlice(nums []int) int {
+	maxNum := nums[0]
+	for _, num := range nums {
+		if num > maxNum {
+			maxNum = num
+		}
+	}
+	return maxNum
+}
