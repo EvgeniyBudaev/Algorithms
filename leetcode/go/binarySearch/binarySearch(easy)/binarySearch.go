4f61afbd35b96a35ce7e3448a2e3a8c398870a6b
Evgeniy Budaev <budaev.e@gmail.com>
2025-03-30 12:40:02 +0300

    feat: 704. Binary Search

---
+++ leetcode/go/binarySearch/binarySearch(easy)/binarySearch.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/binary-search/description/
+
+Учитывая массив целых чисел nums, отсортированный в порядке возрастания, и целочисленную цель, напишите функцию для
+поиска цели в nums. Если цель существует, верните ее индекс. В противном случае верните -1.
+Вы должны написать алгоритм со сложностью выполнения O(log n).
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+*/
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	fmt.Println(search(nums, 9)) // 4
+}
+
+func search(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+
+	for low <= high {
+		mid := (low + high) / 2
+		guess := nums[mid]
+
+		if guess == target {
+			return mid
+		}
+		if guess > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
