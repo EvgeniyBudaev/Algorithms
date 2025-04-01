7eef4badf0ac5f4d54d214770226f6023e0212db
Evgeniy Budaev <budaev.e@gmail.com>
2025-04-01 08:49:57 +0300

    feat: 1323. Maximum 69 Number

---
+++ leetcode/go/greedy/maximum69Number(easy)/maximum69Number.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/* 1323. Maximum 69 Number
+
+Вам дано целое положительное число, состоящее только из цифр 6 и 9.
+Верните максимальное число, которое вы можете получить, изменив не более одной цифры
+(6 становится 9, а 9 становится 6).
+
+Input: num = 9669
+Output: 9969
+Explanation:
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969.
+
+Input: num = 9996
+Output: 9999
+Explanation: Changing the last digit 6 to 9 results in the maximum number.
+
+Input: num = 9999
+Output: 9999
+Explanation: It is better not to apply any change.
+*/
+
+func main() {
+	fmt.Println(maximum69Number(9996)) // 9999
+}
+
+func maximum69Number(num int) int {
+	// Преобразуем число в строку, чтобы работать с отдельными цифрами
+	s := strconv.Itoa(num)
+	// Преобразуем строку в слайс рун для изменения символов
+	digits := []rune(s)
+
+	// Ищем первую 6 и заменяем ее на 9
+	for i, char := range digits {
+		if char == '6' {
+			digits[i] = '9'
+			break // Заменяем только первую 6
+		}
+	}
+
+	// Преобразуем обратно в число
+	result, _ := strconv.Atoi(string(digits))
+	return result
+}
