84bdaeb3d0201be08709d7d4b95afc4a38fa2602
Evgeniy Budaev <budaev.e@gmail.com>
2025-04-02 08:47:30 +0300

    feat: 383. Ransom Note

---
+++ leetcode/go/hashing/ransomNote(easy)/ransomNote.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/* https://leetcode.com/problems/ransom-note/description/
+
+Учитывая две строки ransomNote и magazine, верните true, если ransomNote можно создать с использованием букв из magazine,
+и false в противном случае.
+Каждое письмо в magazine можно использовать в ransomNote только один раз.
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+*/
+
+func main() {
+	fmt.Println(canConstruct("a", "b")) // false
+}
+
+// canConstruct проверяет, можно ли построить строку ransomNote из букв строки magazine
+func canConstruct(ransomNote string, magazine string) bool {
+	// Создаем map для подсчета количества каждой буквы в magazine
+	letterCounts := make(map[rune]int)
+
+	// Заполняем map: ключ - буква, значение - количество таких букв в magazine
+	for _, letter := range magazine {
+		letterCounts[letter]++
+	}
+
+	// Проверяем каждую букву в ransomNote
+	for _, letter := range ransomNote {
+		// Если буквы нет в magazine или ее недостаточно, возвращаем false
+		if count, exists := letterCounts[letter]; !exists || count == 0 {
+			return false
+		}
+		// Уменьшаем количество оставшихся букв
+		letterCounts[letter]--
+	}
+
+	// Если все буквы ransomNote есть в magazine в достаточном количестве
+	return true
+}
