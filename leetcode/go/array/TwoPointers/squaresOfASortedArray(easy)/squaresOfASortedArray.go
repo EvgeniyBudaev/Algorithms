4db57c6283473a0b73c5566905b7075e9cde022f
Evgeniy Budaev <budaev.e@gmail.com>
2025-04-05 07:51:10 +0300

    feat: 977. Squares of a Sorted Array

--- leetcode/go/array/TwoPointers/squaresOfASortedArray(easy)/squaresOfASortedArray.go
+++ leetcode/go/array/TwoPointers/squaresOfASortedArray(easy)/squaresOfASortedArray.go
@@ -27,13 +27,12 @@ func main() {
 	fmt.Println(sortedSquares([]int{-4, -1, 0, 3, 10})) // [0,1,9,16,100]
 }
 
+// sortedSquares возвращает массив квадратов каждого числа, отсортированного в неубывающем порядке.
 func sortedSquares(nums []int) []int {
-	squares := make([]int, len(nums))
-
 	for i, num := range nums {
-		squares[i] = num * num
+		nums[i] = num * num
 	}
 
-	sort.Ints(squares)
-	return squares
+	sort.Ints(nums)
+	return nums
 }
