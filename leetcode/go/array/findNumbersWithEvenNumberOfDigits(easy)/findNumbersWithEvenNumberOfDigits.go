07578192d6298af3db54f0e40d244ecec3779d7f
Evgeniy Budaev <budaev.e@gmail.com>
2025-04-06 07:27:54 +0300

    feat: 1295. Find Numbers with Even Number of Digits

--- leetcode/go/array/findNumbersWithEvenNumberOfDigits(easy)/findNumbersWithEvenNumberOfDigits.go
+++ leetcode/go/array/findNumbersWithEvenNumberOfDigits(easy)/findNumbersWithEvenNumberOfDigits.go
@@ -5,7 +5,8 @@ import (
 	"strconv"
 )
 
-/* https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+/* 1295. Find Numbers with Even Number of Digits
+https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
 
 Найдите числа с четным числом цифр.
 Дан массив целых чисел, верните, сколько из них содержат четное количество цифр.
@@ -20,10 +21,10 @@ Output: 1
 */
 
 func main() {
-	r := findNumbers([]int{12, 345, 2, 6, 7896})
-	fmt.Println(r) // 2
+	fmt.Println(findNumbers([]int{12, 345, 2, 6, 7896})) // 2
 }
 
+// findNumbers возвращает количество чисел с четным числом цифр.
 func findNumbers(nums []int) int {
 	counter := 0
 
