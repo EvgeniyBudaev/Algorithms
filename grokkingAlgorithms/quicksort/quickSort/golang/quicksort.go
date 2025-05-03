9f0df99aed6d79f83d53b01ac0f32a68754db27f
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-03 14:05:43 +0300

    feat: quicksort

---
+++ grokkingAlgorithms/quicksort/quickSort/golang/quicksort.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(quicksort([]int{10, 5, 2, 3})) // [2, 3, 5, 10]
+}
+
+func quicksort(list []int) []int {
+	// Базовый случай
+	if len(list) < 2 {
+		return list
+		// Рекурсивный случай
+	} else {
+		pivot := list[0] // Опорный элемент
+
+		var less = []int{}    // Подмассив всех элементов меньше опорного
+		var greater = []int{} // Подмассив всех элементов больше опорного
+
+		// Фильтруем массив, разбивая его на два подмассива
+		for _, num := range list[1:] {
+			// Если элемент меньше опорного, добавляем его в подмассив "меньше", иначе - в подмассив "больше"
+			if pivot > num {
+				less = append(less, num)
+			} else {
+				greater = append(greater, num)
+			}
+		}
+
+		less = append(quicksort(less), pivot)
+		greater = quicksort(greater)
+		return append(less, greater...)
+	}
+}
