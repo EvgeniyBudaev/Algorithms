6142c69a5b1cac544d730ed92271ad95a16b432e
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-12 07:32:04 +0300

    feat: 17. Letter Combinations of a Phone Number

--- leetcode/go/backtracking/letterCombinationsOfAPhoneNumber(medium)/letterCombinationsOfAPhoneNumber.go
+++ leetcode/go/backtracking/letterCombinationsOfAPhoneNumber(medium)/letterCombinationsOfAPhoneNumber.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-/*
+/* 17. Letter Combinations of a Phone Number
 https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 
 Учитывая строку, содержащую цифры от 2 до 9 включительно, верните все возможные комбинации букв, которые может
@@ -20,7 +20,8 @@ func main() {
 	fmt.Println(letterCombinations("23")) // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 }
 
-// letterCombinations генерирует все возможные комбинации букв, соответствующих цифрам
+// letterCombinations генерирует все возможные комбинации букв, соответствующих цифрам.
+// time: O(4^n) где n - длина входной строки. space: O(n)
 func letterCombinations(digits string) []string {
 	if len(digits) == 0 {
 		return []string{}
@@ -38,10 +39,9 @@ func letterCombinations(digits string) []string {
 		'9': "wxyz",
 	}
 
-	var result []string
-
+	var result []string // Срез для хранения результатов
 	// Внутренняя рекурсивная функция для генерации комбинаций
-	var backtrack func(int, string)
+	var backtrack func(int, string) // Тип функции для рекурсии
 	backtrack = func(index int, combination string) {
 		// Если прошли все цифры, добавляем комбинацию в результат
 		if index == len(digits) {
@@ -50,15 +50,15 @@ func letterCombinations(digits string) []string {
 		}
 
 		// Получаем буквы для текущей цифры
-		currentDigit := digits[index]
-		letters := digitToLetters[currentDigit]
+		currentDigit := digits[index]           // Получаем текущую цифру
+		letters := digitToLetters[currentDigit] // Получаем буквы для текущей цифры
 
 		// Добавляем каждую возможную букву и рекурсивно продолжаем
 		for i := 0; i < len(letters); i++ {
-			backtrack(index+1, combination+string(letters[i]))
+			backtrack(index+1, combination+string(letters[i])) // Рекурсивно вызываем для следующей цифры
 		}
 	}
 
-	backtrack(0, "")
-	return result
+	backtrack(0, "") // Начинаем с первого символа и пустой комбинацией
+	return result    // Возвращаем результат
 }
