6c4d433163a2266826707473bb63598441b4e283
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-12 17:12:11 +0300

    feat: 628. Maximum Product of Three Numbers

---
+++ leetcode/go/array/maximumProductOfThreeNumbers(easy)/maximumProductOfThreeNumbers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/* 628. Maximum Product of Three Numbers
+https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+
+Дан целочисленный массив nums. Найдите три числа, произведение которых максимально, и верните максимальное произведение.
+
+Input: nums = [1,2,3]
+Output: 6
+*/
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(maximumProduct(nums)) // 6
+}
+
+// maximumProduct - функция для нахождения максимального произведения трех чисел в массиве nums/
+// time: O(n * log(n)), space: O(1)
+func maximumProduct(nums []int) int {
+	sort.Ints(nums) // Сортируем массив в порядке возрастания
+	n := len(nums)  // Получаем длину массива
+
+	// Вариант 1: три самых больших числа
+	option1 := nums[n-1] * nums[n-2] * nums[n-3]
+
+	// Вариант 2: два самых маленьких (возможно отрицательных) и самое большое
+	option2 := nums[0] * nums[1] * nums[n-1]
+
+	// Возвращаем максимальное из двух вариантов
+	return max(option1, option2)
+}
