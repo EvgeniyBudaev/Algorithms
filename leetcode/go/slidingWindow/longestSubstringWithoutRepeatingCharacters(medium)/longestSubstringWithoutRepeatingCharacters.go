b86f266cb8386c87c13b628e478ee0dd930a2ea3
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-13 13:08:16 +0300

    feat: 3. Longest Substring Without Repeating Characters

--- leetcode/go/slidingWindow/longestSubstringWithoutRepeatingCharacters(medium)/longestSubstringWithoutRepeatingCharacters.go
+++ leetcode/go/slidingWindow/longestSubstringWithoutRepeatingCharacters(medium)/longestSubstringWithoutRepeatingCharacters.go
@@ -25,26 +25,28 @@ func main() {
 	fmt.Println(lengthOfLongestSubstring("abcabcbb")) // 3
 }
 
-// lengthOfLongestSubstring находит длину самой длинной подстрока без повторения символов.
+// lengthOfLongestSubstring - находит длину самой длинной подстрока без повторения символов.
+// time: O(n), space: O(n)
 func lengthOfLongestSubstring(s string) int {
+	// Если строка пустая или состоит из одного символа, то длина самой длинной подстроки равна длине строки.
 	if len(s) <= 1 {
 		return len(s)
 	}
 
-	left, right, maxLength := 0, 0, 0
-	charMap := make(map[byte]bool)
+	left, right, maxLength := 0, 0, 0 // left - левая граница, right - правая граница, maxLength - максимальная длина подстроки
+	charMap := make(map[byte]bool)    // charMap - карта для хранения уникальных символов
 
 	for right < len(s) {
-		currentChar := s[right]
-		if _, ok := charMap[currentChar]; !ok {
-			charMap[currentChar] = true
-			maxLength = max(maxLength, right-left+1)
-			right++
-		} else {
+		currentChar := s[right]                 // текущий символ
+		if _, ok := charMap[currentChar]; !ok { // если символ не найден в charMap, то добавляем его в charMap
+			charMap[currentChar] = true              // добавляем символ в charMap
+			maxLength = max(maxLength, right-left+1) // вычисляем максимальную длину подстроки
+			right++                                  // сдвигаем правую границу
+		} else { // если символ найден в charMap, то удаляем символ с левой границы и сдвигаем левую границу
 			delete(charMap, s[left])
 			left++
 		}
 	}
 
-	return maxLength
+	return maxLength // возвращаем максимальную длину подстроки
 }
