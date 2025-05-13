48728f8823bf238f0c4efc4b71ae9828ace8de48
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-13 11:02:08 +0300

    feat: Find Max Sum Subarray Length K

--- leetcode/go/slidingWindow/findMaxSumSubarrayLengthK(easy)/findMaxSumSubarrayLengthK.go
+++ leetcode/go/slidingWindow/findMaxSumSubarrayLengthK(easy)/findMaxSumSubarrayLengthK.go
@@ -12,7 +12,8 @@ func main() {
 	fmt.Println(findMaxSumSubarrayLengthK(nums, 4)) // 18 <- [3, -1, 4, 12,]
 }
 
-// findMaxSumSubarrayLengthK находит сумму подмассива с наибольшей суммой, длина которой равна k.
+// findMaxSumSubarrayLengthK - находит сумму подмассива с наибольшей суммой, длина которой равна k.
+// time: O(n), space: O(1)
 func findMaxSumSubarrayLengthK(nums []int, k int) int {
 	sum := 0 // Текущая сумма подмассива длины k
 
@@ -26,10 +27,10 @@ func findMaxSumSubarrayLengthK(nums []int, k int) int {
 	// Скользим окном по массиву
 	for i := k; i < len(nums); i++ {
 		sum += nums[i] - nums[i-k] // Добавляем nums[i] и удаляем nums[i-k]
-		if sum > result {          // Обновляем ans, если текущая сумма больше
+		if sum > result {          // Обновляем result, если текущая сумма больше
 			result = sum
 		}
 	}
 
-	return result
+	return result // Возвращаем наибольшую сумму подмассива
 }
