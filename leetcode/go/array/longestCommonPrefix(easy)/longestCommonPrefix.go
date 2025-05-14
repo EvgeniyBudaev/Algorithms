523f5b0113f6bf53a32eff7dba0c32d17db677ec
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-14 13:17:06 +0300

    feat: 14. Longest Common Prefix

--- leetcode/go/array/longestCommonPrefix(easy)/longestCommonPrefix.go
+++ leetcode/go/array/longestCommonPrefix(easy)/longestCommonPrefix.go
@@ -20,25 +20,25 @@ func main() {
 	fmt.Println(longestCommonPrefix(strs)) // "fl"
 }
 
-// longestCommonPrefix - возвращает самый длинный общий префикс среди массива строк.\
-// time complexity: O(S), где S - суммарная длина всех строк в массиве strs.
-// space complexity: O(1)
+// longestCommonPrefix - возвращает самый длинный общий префикс среди массива строк.
+// time: O(S), где S - суммарная длина всех строк в массиве strs.
+// space: O(1)
 func longestCommonPrefix(strs []string) string {
-	if len(strs) == 0 {
+	if len(strs) == 0 { // Если массив пуст, возвращаем пустую строку.
 		return ""
 	}
 
-	prefix := strs[0]                // "flower"
+	prefix := strs[0]                // Задаем префикс как первую строку. "flower"
 	for i := 1; i < len(strs); i++ { // "flow", "flight"
 		// Проверяем, что длина префикса не превышает длину строки.
 		for strings.Index(strs[i], prefix) != 0 { // strings.Index - возвращает индекс первого вхождения подстроки в строке.
 			// Удаляем последний символ из prefix, пока не найдем общий префикс.
-			prefix = prefix[:len(prefix)-1] // "flowe", "flow", "flo", "fl"
-			if prefix == "" {
+			prefix = prefix[:len(prefix)-1] // Здесь используется срез для получения подстроки. "flowe", "flow", "flo", "fl"
+			if prefix == "" {               // Если префикс стал пустым, возвращаем его.
 				return ""
 			}
 		}
 	}
 
-	return prefix
+	return prefix // Возвращаем найденный префикс.
 }
