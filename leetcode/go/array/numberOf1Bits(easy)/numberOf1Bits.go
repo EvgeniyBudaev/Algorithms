a6aa550c9c7d8d5c1315a1d699e3911c72de2f50
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-14 14:48:47 +0300

    feat: 191. Number of 1 Bits

--- leetcode/go/array/numberOf1Bits(easy)/numberOf1Bits.go
+++ leetcode/go/array/numberOf1Bits(easy)/numberOf1Bits.go
@@ -14,20 +14,19 @@ https://leetcode.com/problems/number-of-1-bits/description/
 
 Input: n = 11
 Output: 3
-Пояснение:
-Входная двоичная строка 1011 имеет всего три установленных бита.
+Пояснение: Входная двоичная строка 1011 имеет всего три установленных бита.
 */
 
 func main() {
 	fmt.Println(hammingWeight(11)) // 3
 }
 
-// hammingWeight принимает положительное целое число и возвращает количество установленных битов в его двоичном
+// hammingWeight - принимает положительное целое число и возвращает количество установленных битов в его двоичном
 // представлении.
 // time: O(log(n)), space: O(log(n))
 func hammingWeight(n int) int {
 	// Преобразовать целое число в двоичное представление
-	binaryRepresentation := strconv.FormatInt(int64(n), 2)
+	binaryRepresentation := strconv.FormatInt(int64(n), 2) // 1011
 
 	// Извлечь двоичное представление в виде строки и подсчитать биты «1»
 	return strings.Count(binaryRepresentation, "1")
