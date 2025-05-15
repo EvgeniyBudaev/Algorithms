17c77e8c6b2f20230a8515b57528f902137a0107
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-15 06:41:37 +0300

    feat: 1426. Counting Elements

--- leetcode/go/hashing/countingElements(easy)/countingElements.go
+++ leetcode/go/hashing/countingElements(easy)/countingElements.go
@@ -35,21 +35,21 @@ func main() {
 	fmt.Println(countElements([]int{1, 1, 2})) // 2
 }
 
-// countElements подсчитывает количество элементов x в массиве, таких что x + 1 также находится в массиве.
+// countElements - подсчитывает количество элементов x в массиве, таких что x + 1 также находится в массиве.
+// time: O(n), space: O(n)
 func countElements(arr []int) int {
-	// Создаем множество (map) для быстрой проверки наличия элементов в массиве.
-	elementSet := make(map[int]bool)
+	elementSet := make(map[int]bool) // Создаем множество (map) для быстрой проверки наличия элементов в массиве.
 	for _, num := range arr {
 		elementSet[num] = true
 	}
 
-	count := 0
+	count := 0 // Счетчик для количества элементов x.
 	// Проходим по всем элементам массива и проверяем, есть ли (x + 1) в множестве.
 	for _, num := range arr {
-		if elementSet[num+1] {
+		if elementSet[num+1] { // Если num + 1 есть в множестве, то увеличиваем счетчик.
 			count++
 		}
 	}
 
-	return count
+	return count // Возвращаем счетчик.
 }
