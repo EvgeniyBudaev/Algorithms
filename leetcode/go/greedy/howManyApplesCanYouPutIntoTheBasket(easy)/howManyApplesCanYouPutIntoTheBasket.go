9e04a6365d65c1a5444eceae0fd962e005a77643
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-15 10:45:06 +0300

    feat: 1196 - How Many Apples Can You Put into the Basket

--- leetcode/go/greedy/howManyApplesCanYouPutIntoTheBasket(easy)/howManyApplesCanYouPutIntoTheBasket.go
+++ leetcode/go/greedy/howManyApplesCanYouPutIntoTheBasket(easy)/howManyApplesCanYouPutIntoTheBasket.go
@@ -7,8 +7,8 @@ import (
 
 /* 1196 - How Many Apples Can You Put into the Basket
 
-У вас есть несколько яблок, где arr[i] — вес i-го яблока. У вас также есть корзина, способная выдержать до 5000 единиц
-веса.
+У вас есть несколько яблок, где arr[i] — вес i-го яблока.
+У вас также есть корзина, способная выдержать до 5000 единиц веса.
 Верните максимальное количество яблок, которое вы можете положить в корзину.
 
 Input: arr = [100,200,150,1000]
@@ -26,20 +26,19 @@ func main() {
 }
 
 // maxNumberOfApples возвращает максимальное количество яблок, которое можно поместить в корзину без превышения веса.
+// time: O(n), space: O(1)
 func maxNumberOfApples(appleWeights []int) int {
-	totalWeight := 0
-	limit := 5000
-	// Сортируем яблоки по весу (от легких к тяжелым)
-	sort.Ints(appleWeights)
+	totalWeight := 0        // Общий вес яблок
+	limit := 5000           // Максимальный вес корзины
+	sort.Ints(appleWeights) // Сортируем яблоки по весу (от легких к тяжелым)
 
-	for i, weight := range appleWeights {
-		totalWeight += weight
+	for i, weight := range appleWeights { // Перебираем все яблоки
+		totalWeight += weight // Добавляем вес текущего яблока к общему весу
 		// Если превысили лимит в 5000, возвращаем текущее количество
 		if totalWeight > limit {
 			return i
 		}
 	}
 
-	// Если все яблоки поместились, возвращаем общее количество
-	return len(appleWeights)
+	return len(appleWeights) // Если все яблоки поместились, возвращаем общее количество
 }
