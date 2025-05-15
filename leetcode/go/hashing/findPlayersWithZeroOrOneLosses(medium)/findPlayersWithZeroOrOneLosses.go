eb386e4df0bd8d044bf8845d40e276be6d4cc976
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-15 06:54:35 +0300

    feat: 2225. Find Players With Zero or One Losses

--- leetcode/go/hashing/findPlayersWithZeroOrOneLosses(medium)/findPlayersWithZeroOrOneLosses.go
+++ leetcode/go/hashing/findPlayersWithZeroOrOneLosses(medium)/findPlayersWithZeroOrOneLosses.go
@@ -5,7 +5,8 @@ import (
 	"sort"
 )
 
-/* https://leetcode.com/problems/find-players-with-zero-or-one-losses/description/
+/* 2225. Find Players With Zero or One Losses
+https://leetcode.com/problems/find-players-with-zero-or-one-losses/description/
 
 Вам дан целочисленный массив matchs, где match[i] = [winneri,loseri] указывает, что игрок-победитель победил
 игрока-проигравшего в матче.
@@ -40,17 +41,19 @@ func main() {
 	fmt.Println(findWinners(matches)) // [[1,2,10],[4,5,7,8]]
 }
 
+// findWinners - функция для нахождения игроков, которые не проиграли ни одного матча или с одним проигрышем.
+// time: O(n), space: O(n), где n - количество матчей
 func findWinners(matches [][]int) [][]int {
-	playerLosses := make(map[int]int)
+	playerLosses := make(map[int]int) // Карта для подсчета проигрышей для каждого игрока
 
 	// Подсчет проигрышей для каждого игрока
-	for _, match := range matches {
-		winner := match[0]
-		loser := match[1]
+	for _, match := range matches { // Проходим по матчам
+		winner := match[0] // Победитель
+		loser := match[1]  // Проигравший
 
 		// Инициализируем победителя, если его нет (только если мы хотим отслеживать всех игроков)
 		if _, exists := playerLosses[winner]; !exists {
-			playerLosses[winner] = 0
+			playerLosses[winner] = 0 // Инициализируем победителя с нулевым количеством проигрышей
 		}
 
 		// Увеличить количество проигрышей проигравшего
@@ -58,23 +61,23 @@ func findWinners(matches [][]int) [][]int {
 	}
 
 	// Подготавливаем фрагменты ответов
-	answer := make([][]int, 2)
+	answer := make([][]int, 2) // 2 фрагмента ответа
 	answer[0] = make([]int, 0) // Игроки с 0 поражениями
 	answer[1] = make([]int, 0) // Игроки с 1 поражением
 
 	// Классифицируем игроков
-	for player, losses := range playerLosses {
-		switch losses {
+	for player, losses := range playerLosses { // Проходим по карте с подсчетом проигрышей
+		switch losses { // Классифицируем игроков по количеству проигрышей
 		case 0:
-			answer[0] = append(answer[0], player)
+			answer[0] = append(answer[0], player) // Добавляем игрока с 0 проигрышами
 		case 1:
-			answer[1] = append(answer[1], player)
+			answer[1] = append(answer[1], player) // Добавляем игрока с 1 проигрышем
 		}
 	}
 
 	// Сортируем результаты
-	sort.Ints(answer[0])
-	sort.Ints(answer[1])
+	sort.Ints(answer[0]) // Сортируем игроков с 0 проигрышами
+	sort.Ints(answer[1]) // Сортируем игроков с 1 проигрышем
 
-	return answer
+	return answer // Возвращаем ответ
 }
