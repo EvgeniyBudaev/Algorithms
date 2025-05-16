17ea5c267f0a220d6d27ffd4b886162ed6d2c7f7
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-16 07:10:17 +0300

    feat: 1346. Check If N and Its Double Exist

--- leetcode/go/twoPointers/checkIfNAndItsDoubleExist(easy)/checkIfNAndItsDoubleExist.go
+++ leetcode/go/twoPointers/checkIfNAndItsDoubleExist(easy)/checkIfNAndItsDoubleExist.go
@@ -23,22 +23,22 @@ func main() {
 	fmt.Println(checkIfExist([]int{10, 2, 5, 3})) // true
 }
 
-// checkIfExist - проверяет, существует ли в массиве arr два различных индекса i и j,
+// checkIfExist проверяет, существует ли в массиве arr два различных индекса i и j,
 // таких что один элемент равен удвоенному другому (т.е. arr[i] == 2 * arr[j] или arr[j] == 2 * arr[i]).
 // time: O(n^2), space: O(1)
 func checkIfExist(arr []int) bool {
-	left, right := 0, 1
+	left, right := 0, 1 // инициализируем два указателя на начало и конец массива
 
-	for left < len(arr)-1 {
-		if arr[left] == arr[right]*2 || arr[right] == arr[left]*2 {
-			return true
-		} else if right == len(arr)-1 {
-			left++
+	for left < len(arr)-1 { // запускаем цикл для перемещения левого указателя
+		if arr[left] == arr[right]*2 || arr[right] == arr[left]*2 { // проверяем условие на равенство
+			return true // если условие выполняется, возвращаем true
+		} else if right == len(arr)-1 { // если достигнут конец массива, перемещаем левый указатель вправо и обновляем правый
+			left++ // двигаем левый указатель вправо
 			right = left + 1
-		} else {
+		} else { // перемещаем правый указатель вправо
 			right++
 		}
 	}
 
-	return false
+	return false // если не найдено ни одной пары, возвращаем false
 }
