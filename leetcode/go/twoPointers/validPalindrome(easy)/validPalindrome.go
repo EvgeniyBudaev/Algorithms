6a35682fb682b184f79a47fdd498368112d143be
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-17 08:35:52 +0300

    feat: 125. Valid Palindrome

--- leetcode/go/twoPointers/validPalindrome(easy)/validPalindrome.go
+++ leetcode/go/twoPointers/validPalindrome(easy)/validPalindrome.go
@@ -14,29 +14,29 @@ https://leetcode.com/problems/valid-palindrome/description/
 
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
-Explanation: "amanaplanacanalpanama" is a palindrome.
+Объяснение: "amanaplanacanalpanama" палиндром.
 
 Input: s = "race a car"
 Output: false
-Explanation: "raceacar" is not a palindrome.
+Объяснение: "raceacar" не палиндром.
 
 Input: s = " "
 Output: true
-Explanation: s is an empty string "" after removing non-alphanumeric characters.
-Since an empty string reads the same forward and backward, it is a palindrome.
+Объяснение: s — это пустая строка "" после удаления небуквенно-цифровых символов.
+Поскольку пустая строка читается одинаково и вперед, и назад, она является палиндромом.
 */
 
 func main() {
 	fmt.Println(isPalindrome("A man, a plan, a canal: Panama")) // true
 }
 
-// isAlphaNumeric - проверяет, является ли переданный символ буквенно-цифровым
+// isAlphaNumeric проверяет, является ли переданный символ буквенно-цифровым
 // time: O(1), space: O(1)
 func isAlphaNumeric(c rune) bool {
 	return unicode.IsLetter(c) || unicode.IsDigit(c)
 }
 
-// isPalindrome - проверяет, является ли строка s палиндромом после нормализации.
+// isPalindrome проверяет, является ли строка s палиндромом после нормализации.
 // time: O(n), где n - количество символов в строке, space: O(1)
 func isPalindrome(s string) bool {
 	left, right := 0, len(s)-1
@@ -64,5 +64,5 @@ func isPalindrome(s string) bool {
 		right--
 	}
 
-	return true
+	return true // строка является палиндромом
 }
