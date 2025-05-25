75969565ba73c829252d6d816934810a512f5d16
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-25 07:53:51 +0300

    feat: 1. Two Sum

--- leetcode/go/array/twoSum(easy)/twoSum.go
+++ leetcode/go/array/twoSum(easy)/twoSum.go
@@ -11,16 +11,13 @@ https://leetcode.com/problems/two-sum/description/
 target. Вы можете предположить, что каждый вход будет иметь ровно одно решение, и вы не можете использовать один и тот
 же элемент дважды. Вы можете вернуть ответ в любом порядке.
 
-Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
 
-Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 */
@@ -29,24 +26,20 @@ func main() {
 	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // [0,1]
 }
 
-// twoSum - возвращает индексы двух чисел так, чтобы их сумма составляла target.
+// twoSum возвращает индексы двух чисел так, чтобы их сумма составляла target.
 // time: O(n), space: O(n)
 func twoSum(nums []int, target int) []int {
-	// Создаем map для хранения чисел и их индексов
-	numMap := make(map[int]int)
+	numMap := make(map[int]int) // Создаем map для хранения чисел и их индексов
 
 	for i, num := range nums {
-		// Вычисляем разницу между целевым значением и текущим числом
-		diff := target - num
+		diff := target - num // Вычисляем разницу между целевым значением и текущим числом
 		// Проверяем, есть ли разница в map
 		if index, ok := numMap[diff]; ok {
 			// Если есть, возвращаем индексы
 			return []int{index, i}
 		}
-		// Сохраняем текущее число и его индекс в map
-		numMap[num] = i
+		numMap[num] = i // Сохраняем текущее число и его индекс в map
 	}
 
-	// Если решение не найдено, возвращаем nil
-	return nil
+	return nil // Если решение не найдено, возвращаем nil
 }
