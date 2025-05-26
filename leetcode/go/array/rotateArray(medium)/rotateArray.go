530b6e02f1204d0716d5135306cb207b2b4bcca3
Evgeniy Budaev <budaev.e@gmail.com>
2025-05-26 07:40:28 +0300

    feat: 189. Rotate Array

--- leetcode/go/array/rotateArray(medium)/rotateArray.go
+++ leetcode/go/array/rotateArray(medium)/rotateArray.go
@@ -9,7 +9,6 @@ https://leetcode.com/problems/rotate-array/description/
 
 Дан целочисленный массив nums. Повернуть массив вправо на k шагов, где k — неотрицательное число.
 
-Example 1:
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4]
 Explanation:
@@ -17,7 +16,6 @@ rotate 1 steps to the right: [7,1,2,3,4,5,6]
 rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-Example 2:
 Input: nums = [-1,-100,3,99], k = 2
 Output: [3,99,-1,-100]
 Explanation:
@@ -37,29 +35,27 @@ func main() {
 	fmt.Println(nums2) // [3,99,-1,-100]
 }
 
-// rotate — поворачивает массив от индекса 0 до индекса len(nums)-1.
-// time complexity: O(n) — проходим по всем элементам.
-// space complexity: O(1) — не используем дополнительную память.
+// rotate поворачивает массив от индекса 0 до индекса len(nums)-1.
+// time: O(n) — проходим по всем элементам, space: O(1) — не используем дополнительную память.
 func rotate(nums []int, k int) {
-	n := len(nums)
-	k = k % n // Нормализуем k, чтобы он был в пределах длины массива
-	if k == 0 {
-		return // Если k = 0, вращение не нужно
+	n := len(nums) // Длина массива
+	k = k % n      // Нормализуем k, чтобы он был в пределах длины массива
+	if k == 0 {    // Если k = 0, вращение не нужно
+		return
 	}
 	rotateSubArray(nums, 0, n) // Поворачиваем весь массив. // [7, 6, 5, 4, 3, 2, 1]
 	rotateSubArray(nums, 0, k) // [5, 6, 7, 4, 3, 2, 1]
 	rotateSubArray(nums, k, n) // [5, 6, 7, 1, 2, 3, 4]
 }
 
-// rotateSubArray — поворачивает массив от индекса i до индекса j (не включительно).
-// time complexity: O(n) — проходим по всем элементам.
-// space complexity: O(1) — не используем дополнительную память.
+// rotateSubArray поворачивает массив от индекса i до индекса j (не включительно).
+// time: O(n) — проходим по всем элементам, space: O(1) — не используем дополнительную память.
 func rotateSubArray(nums []int, i, j int) []int {
-	j -= 1 // ЧТобы не включать последний индекс в поворот.
-	for i < j {
+	j -= 1      // ЧТобы не включать последний индекс в поворот
+	for i < j { // Пока индексы не встретятся
 		nums[i], nums[j] = nums[j], nums[i]
 		i++
 		j--
 	}
-	return nums
+	return nums // Возвращаем измененный массив
 }
