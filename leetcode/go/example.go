bbccf3e27e16fb778ede990e2eb6e1a5def72421
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-03 17:22:59 +0300

    feat: codewars refactoring

--- leetcode/go/example.go
+++ leetcode/go/example.go
@@ -3,17 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// предложение
-	sentence := "Πολύ λίγα πράγματα συμβαίνουν στο σωστό χρόνο, και τα υπόλοιπα δεν συμβαίνουν καθόλου"
-	// инициализируем map
-	// ключами будут знаки, а значениями — количество вхождений
-	frequency := make(map[rune]int)
-	// пройдёмся по знакам в предложении
-	for _, v := range sentence {
-		frequency[v]++ // встреченному знаку увеличиваем частоту
-	}
-	// печатаем
-	for k, v := range frequency {
-		fmt.Printf("Знак %c встречается %d раз \n", k, v)
-	}
+	ch := make(chan int)
+	go func() {
+		ch <- 10
+		defer close(ch)
+	}()
+	v, closed := <-ch
+	fmt.Println(v, closed)
+	v, closed = <-ch
+	fmt.Println(v, closed)
 }
