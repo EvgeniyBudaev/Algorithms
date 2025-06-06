f076420d6e3f4fbf2db68bf8aa506ab5f4381af7
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-06 17:19:32 +0300

    feat: Duplicate Encoder

--- leetcode/go/example.go
+++ leetcode/go/example.go
@@ -1,15 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	ch := make(chan int)
-	go func() {
-		ch <- 10
-		defer close(ch)
-	}()
-	v, closed := <-ch
-	fmt.Println(v, closed)
-	v, closed = <-ch
-	fmt.Println(v, closed)
+	arr := []int{0, 3, 2, 1}
+	fmt.Println(validMountainArray(arr)) // true
+}
+
+func validMountainArray(arr []int) bool {
+	if len(arr) < 2 {
+		return false
+	}
+	left, right := 0, len(arr)-1
+	for i := 0; i < len(arr); i++ {
+		if arr[left] < arr[left+1] {
+			left++
+		}
+		if arr[right] < arr[right-1] {
+			right--
+		}
+		if left == 0 || right == len(arr)-1 {
+			return false
+		}
+		if left == right {
+			return true
+		}
+	}
+	return false
 }
