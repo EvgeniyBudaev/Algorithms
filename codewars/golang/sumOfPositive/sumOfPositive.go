527fd182a152c56a4bb538ab9843f93ea7e61d46
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-10 08:51:37 +0300

    feat: Sum of positive

---
+++ codewars/golang/sumOfPositive/sumOfPositive.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+/* Sum of positive
+https://www.codewars.com/kata/5715eaedb436cf5606000381/train/go
+
+Получаете массив чисел, возвращаете сумму всех положительных единиц.
+
+Example:
+[1, -4, 7, 12] => 1 + 7 + 12 = 20
+*/
+
+func main() {
+	numbers := []int{1, -4, 7, 12}
+	fmt.Println(PositiveSum(numbers)) // 20
+}
+
+func PositiveSum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		if number > 0 {
+			sum += number
+		}
+	}
+
+	return sum
+}
