be4f1f3c91cd8b6e4090ecb130ab7b45a5e89606
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-11 09:11:39 +0300

    feat: Remove First and Last Character

---
+++ codewars/golang/ removeFirstAndLastCharacter/removeFirstAndLastCharacter.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* Remove First and Last Character
+https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/go
+
+Это довольно просто. Ваша цель — создать функцию, которая удаляет первый и последний символы строки.
+Вам дан один параметр — исходная строка. Вам не нужно беспокоиться о строках, содержащих менее двух символов.
+*/
+
+func main() {
+	fmt.Println(RemoveChar("country")) // "ountr"
+}
+
+func RemoveChar(word string) string {
+	return word[1 : len(word)-1]
+}
