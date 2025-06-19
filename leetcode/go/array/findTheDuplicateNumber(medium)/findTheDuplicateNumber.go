eadad79d680c7d5a312eeb3442e5a49e87fdbec1
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-19 07:54:23 +0300

    feat: 287. Find the Duplicate Number

--- leetcode/go/array/findTheDuplicateNumber(medium)/findTheDuplicateNumber.go
+++ leetcode/go/array/findTheDuplicateNumber(medium)/findTheDuplicateNumber.go
@@ -27,21 +27,18 @@ func main() {
 // time: O(n), space: O(1)
 func findDuplicate(nums []int) int {
 	for _, num := range nums {
-		i := abs(num) // индекс в массиве
-		// Если элемент уже посещался, значит это повторяющееся число.
-		if nums[i] < 0 {
+		i := abs(num)    // индекс в массиве
+		if nums[i] < 0 { // Если элемент уже посещался, значит это повторяющееся число.
 			return i
-			// Если элемент еще не посещался, сделаем его отрицательным.
-		} else {
+		} else { // Если элемент еще не посещался, сделаем его отрицательным.
 			nums[i] = -nums[i]
 		}
 	}
 
-	// Не найдено повторяющееся число.
-	return 0
+	return 0 // Не найдено повторяющееся число.
 }
 
-// abs - возвращает абсолютное значение числа.
+// abs возвращает абсолютное значение числа.
 // time: O(1), space: O(1)
 func abs(a int) int {
 	if a < 0 {
@@ -49,33 +46,3 @@ func abs(a int) int {
 	}
 	return a
 }
-
-// findDuplicate - возвращает целое повторяющееся число.
-// time complexity: O(n) - проход по массиву.
-// space complexity: O(n) - используется вспомогательный массив для хранения посещенных чисел.
-//func findDuplicate(nums []int) int {
-//	visited := make([]bool, len(nums))
-//
-//	for _, num := range nums {
-//		if !visited[num] {
-//			visited[num] = true
-//		} else {
-//			return num
-//		}
-//	}
-//
-//	return -1
-//}
-
-// findDuplicate - возвращает целое повторяющееся число.
-// time: O(n*log(n)) - сортировка массива.
-// space: O(1) - не используется дополнительная память.
-//func findDuplicate(nums []int) int {
-//	sort.Ints(nums)
-//	for i := 1; i < len(nums); i++ {
-//		if nums[i] == nums[i-1] {
-//			return nums[i]
-//		}
-//	}
-//	return -1
-//}
