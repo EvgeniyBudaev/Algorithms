a041525d7e4630300a6a0bc73707ab01888abf0b
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-26 09:18:02 +0300

    feat: fibonacci

--- yandex/fibonacci/fibonacci.go
+++ yandex/fibonacci/fibonacci.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(Fib(3)) // 2
 }
 
+// Рекурсивный вариант.
 // Fib вычисляет n-ое число Фибоначчи.
 // time: O(2^n), space: O(n), где n - порядковый номер числа
 func Fib(n int) int {
@@ -16,3 +17,15 @@ func Fib(n int) int {
 		return Fib(n-1) + Fib(n-2)
 	}
 }
+
+// Итеративный вариант.
+// Fib вычисляет n-ое число Фибоначчи.
+// time: O(n), space: O(1), где n - порядковый номер числа
+//func Fib(n int) int {
+//	a, b := 0, 1
+//	for n > 0 {
+//		a, b = b, a+b
+//		n--
+//	}
+//	return a
+//}
