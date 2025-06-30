965099672e97a9f7f74c22ffc854a8eb43851b73
Evgeniy Budaev <budaev.e@gmail.com>
2025-06-30 09:30:00 +0300

    feat: What is between?

--- codewars/golang/whatIsBetween/whatIsBetween.go
+++ codewars/golang/whatIsBetween/whatIsBetween.go
@@ -20,6 +20,8 @@ func main() {
 	fmt.Println(Between(1, 4)) // [1, 2, 3, 4]
 }
 
+// Between возвращает массив всех целых чисел между двумя входными параметрами, включая их самих.
+// time: O(n), space: O(n), где n - разница между a и b
 func Between(a, b int) []int {
 	var result []int
 
