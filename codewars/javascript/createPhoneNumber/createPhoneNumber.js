5c5ab89972235954402999909dba288a2a8d2234
Evgeniy Budaev <budaev.e@gmail.com>
2025-09-11 17:25:30 +0300

    feat: createPhoneNumber

--- codewars/javascript/createPhoneNumber/createPhoneNumber.js
+++ codewars/javascript/createPhoneNumber/createPhoneNumber.js
@@ -1,5 +1,5 @@
-const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
+// createPhoneNumber возвращает строку, которая представляет собой номер телефона в формате (xxx) xxx-xxxx.
+// time: O(n), space: O(n), n - длина массива
 function createPhoneNumber(numbers){
   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`;
 }
@@ -15,4 +15,5 @@ function createPhoneNumber(numbers){
 // 	return format;
 // }
 
+const numbers= [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 console.log(createPhoneNumber(numbers)); // "(123) 456-7890"
