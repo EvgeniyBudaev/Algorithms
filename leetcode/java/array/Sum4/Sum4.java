7ef42f79c11f2c5258034944a94f0cd6c4cfea3d
Evgeniy Budaev <budaev.e@gmail.com>
2025-10-03 16:06:39 +0300

    feat: 18. 4Sum

---
+++ leetcode/java/array/Sum4/Sum4.java
@@ -0,0 +1,78 @@
+package array.Sum4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 18. 4Sum
+https://leetcode.com/problems/4sum/description/
+
+Дан массив nums из n целых чисел, вернуть массив всех уникальных четверок [nums[a], nums[b], nums[c], nums[d]] таких, что:
+
+0 <= a, b, c, d < n
+a, b, c и d различны.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+Вы можете вернуть ответ в любом порядке.
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+*/
+
+public class Sum4 {
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        System.out.println(fourSum(nums, target)); // [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    }
+
+    // fourSum - возвращает массив всех уникальных четверок [nums[a], nums[b], nums[c], nums[d]] таких, что nums[a] + nums[b] + nums[c] + nums[d] == target.
+    // time: O(n^3), space: O(n)
+    private static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums); // Сортируем массив для удобства работы с двумя указателями [-2,-1,0,0,1,2]
+        List<List<Integer>> result = new ArrayList<>(); // Результат
+        int n = nums.length; // Длина массива
+
+        for (int i = 0; i < n - 3; i++) { // Первый указатель
+            // Пропускаем дубликаты для первого числа
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n - 2; j++) { // Второй указатель
+                // Пропускаем дубликаты для второго числа
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1; // Левый указатель
+                int right = n - 1; // Правый указатель
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right]; // Сумма
+
+                    if (sum == target) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        // Пропускаем дубликаты для третьего числа
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        // Пропускаем дубликаты для четвертого числа
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                        // Если сумма меньше целевого значения, двигаем левый указатель вправо
+                    } else if (sum < target) {
+                        left++;
+                        // Если сумма больше целевого значения, двигаем правый указатель влево
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+
+        return result; // Возвращаем список всех уникальных четверок
+    }
+}
