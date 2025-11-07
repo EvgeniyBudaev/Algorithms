fa7a706407223ebb0e436cfcaa5bbc4c62c2dcf4
Evgeniy Budaev <budaev.e@gmail.com>
2025-11-07 07:02:47 +0300

    feat: 27. Remove Element

---
+++ leetcode/java/array/RemoveElement/RemoveElement.java
@@ -0,0 +1,40 @@
+package array.RemoveElement;
+
+/* 27. Remove Element
+https://leetcode.com/problems/remove-element/description/
+
+Учитывая целочисленный массив nums и целочисленное значение, удалите все вхождения val в nums на месте. Порядок
+элементов может быть изменен. Затем верните количество элементов в виде чисел, которые не равны val.
+Учитывайте количество элементов в nums, которые не равны val be k. Чтобы вас приняли, вам необходимо сделать следующее:
+Измените массив nums так, чтобы первые k элементов nums содержали элементы, не равные val. Остальные элементы nums не
+важны, как и размер nums.
+Вернуть К.
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+*/
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(removeElement(nums, 3)); // 2
+    }
+
+    // removeElement удаляет все вхождения val в nums.
+    // time: O(n), space: O(1)
+    private static int removeElement(int[] nums, int val) {
+        int count = 0; // Счетчик для отслеживания элементов, отличных от val
+
+        for (int i = 0; i < nums.length; i++) { // Перебираем элементы массива
+            if (nums[i] != val) { // Если текущий элемент не равен val, то переносим его в начало массива
+                nums[count] = nums[i]; // Перемещаем элемент в начало массива
+                count++; // Увеличиваем счетчик
+            }
+        }
+
+        return count; // Возвращаем фактическую длину
+    }
+}
