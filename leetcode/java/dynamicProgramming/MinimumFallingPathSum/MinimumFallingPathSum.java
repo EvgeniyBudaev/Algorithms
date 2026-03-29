061cb83ddcc90f42f10cbaa1aea6207a4914d07c
Evgeniy Budaev <budaev.e@gmail.com>
2026-03-29 14:04:10 +0300

    feat: 931. Minimum Falling Path Sum

---
+++ leetcode/java/dynamicProgramming/MinimumFallingPathSum/MinimumFallingPathSum.java
@@ -0,0 +1,26 @@
+package leetcode.java.dynamicProgramming.MinimumFallingPathSum;
+
+/* 931. Minimum Falling Path Sum
+https://leetcode.com/problems/minimum-falling-path-sum/description/
+
+Дана матрица n x n целых чисел, вернуть минимальную сумму любого нисходящего пути через матрицу.
+
+Нисходящий путь начинается с любого элемента в первой строке и выбирает элемент в следующей строке, который находится
+либо непосредственно под ним, либо по диагонали слева/справа. В частности, следующим элементом из позиции (row, col)
+будет (row + 1, col - 1), (row + 1, col) или (row + 1, col + 1).
+
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Пояснение: Существуют два падающих пути с минимальной суммой, как показано на рисунке.
+*/
+
+public class MinimumFallingPathSum {
+    public static void main(String[] args) {
+        
+    }
+
+    // minFallingPathSum вычисляет минимальную сумму падающего пути в матрице
+    private static int minFallingPathSum(int[][] matrix) {
+        
+    }
+}
