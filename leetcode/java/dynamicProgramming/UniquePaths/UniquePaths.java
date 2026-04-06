ef9bce3932fd452cfac20efbb5da1dcc4a07e7fc
Evgeniy Budaev <budaev.e@gmail.com>
2026-04-06 06:36:04 +0300

    feat: 62. Unique Paths

---
+++ leetcode/java/dynamicProgramming/UniquePaths/UniquePaths.java
@@ -0,0 +1,57 @@
+package leetcode.java.dynamicProgramming.UniquePaths;
+
+/* 62. Unique Paths
+https://leetcode.com/problems/unique-paths/description/
+
+На сетке m x n находится робот. Робот изначально расположен в верхнем левом углу (т. е. сетка[0][0]). Робот пытается
+переместиться в правый нижний угол (т. е. сетку[m - 1][n - 1]). В любой момент времени робот может двигаться только вниз
+или вправо.
+
+Учитывая два целых числа m и n, верните количество возможных уникальных путей, по которым робот может добраться до
+правого нижнего угла.
+
+Тестовые случаи генерируются так, что ответ будет меньше или равен 2 * 109.
+
+Input: m = 3, n = 7
+Output: 28
+
+Input: m = 3, n = 2
+Output: 3
+Пояснение: Из верхнего левого угла можно попасть в правый нижний угол тремя способами:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+*/
+
+public class UniquePaths {
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7)); // 28
+    }
+
+    // uniquePaths вычисляет количество уникальных путей из верхнего левого угла.
+    // time: O(m * n), где m - количество строк, n - количество столбцов
+    // space: O(n) - используем два массива размером n
+    private static int uniquePaths(int m, int n) {
+        int[] aboveRow = new int[n]; // Создаем временный массив для хранения промежуточных результатов
+
+        // Инициализируем первую строку единицами
+        for (int i = 0; i < n; i++) {
+            aboveRow[i] = 1;
+        }
+
+        // Заполняем последующие строки
+        for (int row = 1; row < m; row++) {
+            int[] currentRow = new int[n]; // Создаем новый временный массив для текущей строки
+            currentRow[0] = 1;             // Первый элемент в строке всегда 1
+
+            for (int col = 1; col < n; col++) {
+                // Количество путей равно сумме путей сверху и слева
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+
+            aboveRow = currentRow; // Обновляем aboveRow для следующей итерации
+        }
+
+        return aboveRow[n - 1]; // Возвращаем количество путей до правого нижнего угла
+    }
+}
