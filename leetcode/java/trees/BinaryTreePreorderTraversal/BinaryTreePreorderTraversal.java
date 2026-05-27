46ed72530f25bd811bdb4fa21bb94b54edcc512b
Evgeniy Budaev <budaev.e@gmail.com>
2026-05-27 07:55:23 +0300

    feat: 144. Binary Tree Preorder Traversal

---
+++ leetcode/java/trees/BinaryTreePreorderTraversal/BinaryTreePreorderTraversal.java
@@ -0,0 +1,96 @@
+package leetcode.java.trees.BinaryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/* 144. Binary Tree Preorder Traversal
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+Для заданного корня бинарного дерева вернуть прямой обход значений его узлов.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+*/
+
+public class BinaryTreePreorderTraversal {
+    public static void main(String[] args) {
+        TreeNode root1 = createTree(new Integer[]{1, null, 2, 3});
+        System.out.println(preorderTraversal(root1)); // [1, 2, 3]
+
+        TreeNode root2 = createTree(new Integer[]{1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9});
+        System.out.println(preorderTraversal(root2)); // [1, 2, 4, 5, 6, 7, 3, 8, 9]
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // preorderTraversal возвращает прямой обход значений его узлов.
+    // time: O(n), где n - количество узлов в дереве
+    // space: O(h), где h - высота дерева
+    private static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
+    }
+
+    // traverse рекурсивно проходит по дереву и добавляет значения узлов в ответ.
+    // time: O(n), space: O(h)
+    private static void traverse(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        // Pre order - значит перед тем как рекурсивно идти в левое и правое поддерево 
+        // мы добавляем значение узла в ответ.
+        result.add(node.val);                    // Добавляем значение текущего узла в ответ
+        traverse(node.left, result);             // Добавляем значение левого потомка
+        traverse(node.right, result);            // Добавляем значение правого потомка
+    }
+
+    // createTree создает бинарное дерево из массива значений.
+    private static TreeNode createTree(Integer[] values) {
+        if (values == null || values.length == 0 || values[0] == null) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(values[0]); // Создаем корневой узел с первым значением в массиве
+        Queue<TreeNode> queue = new LinkedList<>(); // Создаем очередь для обхода дерева по уровням
+        queue.offer(root);
+        int i = 1; // Индекс для обхода массива значений
+
+        while (!queue.isEmpty() && i < values.length) {
+            TreeNode node = queue.poll(); // Берем первый элемент из очереди
+
+            // Левый потомок
+            if (i < values.length && values[i] != null) {
+                node.left = new TreeNode(values[i]); // Создаем левый потомок и добавляем его в очередь
+                queue.offer(node.left); // Добавляем левого потомка в очередь
+            }
+            i++;
+
+            // Правый потомок
+            // Если правый потомок не равен null, то создаем его и добавляем его в очередь
+            if (i < values.length && values[i] != null) {
+                node.right = new TreeNode(values[i]); // Создаем правого потомка и добавляем его в очередь
+                queue.offer(node.right); // Добавляем правого потомка в очередь
+            }
+            i++;
+        }
+
+        // Возвращаем ссылку на корневой узел
+        return root;
+    }
+}
