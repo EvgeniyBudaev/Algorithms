aadf2024ea910084e952dec5e3c396b871049c64
Evgeniy Budaev <budaev.e@gmail.com>
2026-07-02 06:11:55 +0300

    feat: checkIfPalindrome

---
+++ leetcode/javascript/twoPointers/checkIfPalindrome(easy)/checkIfPalindrome.js
@@ -0,0 +1,22 @@
+/*
+Учитывая строку s, верните true, если это палиндром, и false в противном случае.
+Строка является палиндромом, если она читается как вперед, так и назад одинаково.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkIfPalindrome = function (s) {
+    let left = 0, right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+};
