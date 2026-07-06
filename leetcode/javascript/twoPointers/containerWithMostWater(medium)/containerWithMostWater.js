9de5e6300c0052e2a9420ba0847c6f80988afd8b
Evgeniy Budaev <budaev.e@gmail.com>
2026-07-06 09:17:47 +0300

    feat: 11. Container With Most Water

---
+++ leetcode/javascript/twoPointers/containerWithMostWater(medium)/containerWithMostWater.js
@@ -0,0 +1,46 @@
+/* 11. Container With Most Water
+https://leetcode.com/problems/container-with-most-water/description/
+
+Вам дан целочисленный массив высотой длины n. Нарисовано n вертикальных линий, конечными точками которых являются
+(i, 0) и (i, height[i]).
+Найдите две линии, которые вместе с осью X образуют контейнер, в котором содержится больше всего воды.
+Верните максимальное количество воды, которое может хранить контейнер.
+Обратите внимание, что вы не можете наклонять контейнер
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Пояснение: Вышеупомянутые вертикальные линии представлены массивом [1,8,6,2,5,4,8,3,7].
+В этом случае максимальная площадь воды (синяя секция), которую может содержать контейнер, равна 49.
+
+Input: height = [1,1]
+Output: 1
+*/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let maxAreaContainer = 0; // Максимальная площадь воды
+    let left = 0;             // Левый указатель
+    let right = height.length - 1; // Правый указатель
+
+    while (left < right) {
+        // Находим минимальную высоту между двумя линиями
+        const minHeight = Math.min(height[left], height[right]);
+        // Вычисляем ширину
+        const width = right - left;
+        // Вычисляем площадь и обновляем maxArea, если текущая площадь больше
+        const currentArea = width * minHeight;
+        maxAreaContainer = Math.max(maxAreaContainer, currentArea);
+
+        // Перемещаем указатель с меньшей высотой
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxAreaContainer; // Возвращаем максимальную площадь
+};
