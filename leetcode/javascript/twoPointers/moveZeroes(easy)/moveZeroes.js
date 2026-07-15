97dca6d500b39a6d6ebaebc3831239b383ca195d
Evgeniy Budaev <budaev.e@gmail.com>
2026-07-15 09:00:41 +0300

    feat: 283. Move Zeroes

---
+++ leetcode/javascript/twoPointers/moveZeroes(easy)/moveZeroes.js
@@ -0,0 +1,29 @@
+/* 283. Move Zeroes
+https://leetcode.com/problems/move-zeroes/description/
+
+Учитывая целочисленный массив nums, переместите все 0 в его конец, сохраняя относительный порядок ненулевых элементов.
+Обратите внимание, что вы должны сделать это на месте, не копируя массив.
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Input: nums = [0]
+Output: [0]
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let left = 0; // Указатель для позиции, куда ставить следующий ненулевой элемент
+
+    // Проходим по массиву правым указателем
+    for (let right = 0; right < nums.length; right++) {
+        // Если нашли ненулевой элемент, меняем его местами с элементом на позиции left
+        if (nums[right] !== 0) {
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
+        }
+    }
+};
