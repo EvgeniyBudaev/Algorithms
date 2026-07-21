286524831e153ca290a0378d7cc319e37e463751
Evgeniy Budaev <budaev.e@gmail.com>
2026-07-21 08:55:05 +0300

    feat: 344. Reverse String

---
+++ leetcode/javascript/twoPointers/reverseString(easy)/reverseString.js
@@ -0,0 +1,26 @@
+/* 344. Reverse String
+https://leetcode.com/problems/reverse-string/description/
+
+Напишите функцию, которая переворачивает строку. Входная строка задается как массив символов s.
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+};
