f2922b0b6dd39b981350ac548a2b3044469d755f
Evgeniy Budaev <budaev.e@gmail.com>
2026-08-08 08:16:58 +0300

    feat: Training

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,5 +1,35 @@
 
 public class Training {
     public static void main(String[] args) {
+        Rodent capybara = new Capybara();
+        capybara.crunch();
+        capybara.sleep();
+    }
+}
+
+class Rodent {
+    public void crunch() {
+        System.out.println("Грызун грызёт что угодно.");
+    }
+
+    public void sleep() {
+        System.out.println("Грызун уснул.");
+    }
+}
+
+class Capybara extends Rodent {
+
+    @Override
+    public void crunch() {
+        System.out.println("Капибара любит грызть кукурузу.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Капибара крепко спит.");
+    }
+
+    public void dive() {
+        System.out.println("Капибара умеет нырять.");
     }
 }
