c0ddcbcff2abc2402bfa46eaf116811318c63627
Evgeniy Budaev <budaev.e@gmail.com>
2026-08-22 07:41:52 +0300

    feat: 977. Squares of a Sorted Array

--- leetcode/java/twoPointers/SquaresOfASortedArray/SquaresOfASortedArray.java
+++ leetcode/java/twoPointers/SquaresOfASortedArray/SquaresOfASortedArray.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 /* 977. Squares of a Sorted Array
 https://leetcode.com/problems/squares-of-a-sorted-array/description/
 
-Учитывая целочисленный массив nums, отсортированный в неубывающем порядке, верните массив квадратов каждого числа,
-отсортированного в неубывающем порядке.
+Учитывая целочисленный массив nums, отсортированный в неубывающем порядке,
+верните массив квадратов каждого числа, отсортированного в неубывающем порядке.
 
 Input: nums = [-4,-1,0,3,10]
 Output: [0,1,9,16,100]
@@ -54,17 +54,6 @@ public class SquaresOfASortedArray {
 
         return result;
     }
-
-    // sortedSquares возвращает массив квадратов каждого числа, отсортированного в неубывающем порядке.
-    // time: O(n), space: O(n)
-//    private int[] sortedSquares(int[] nums) {
-//        for (int i = 0; i < nums.length; i++) {
-//            nums[i] = nums[i] * nums[i];
-//        }
-//
-//        Arrays.sort(nums);
-//        return nums;
-//    }
 
     // sortedSquares возвращает массив квадратов каждого числа, отсортированного в неубывающем порядке.
     // time: O(n), space: O(n)
