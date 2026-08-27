cc6796d9c0d5ad3dca7eb639b3eafd6dd2c3dba8
Evgeniy Budaev <budaev.e@gmail.com>
2026-08-27 07:37:35 +0300

    feat: isPalindrome

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,19 +1,36 @@
+
 public class Training {
     public static void main(String[] args) {
-        System.out.println(isSubsequence("ace", "abcde")); // true
-        System.out.println(isSubsequence("aec", "abcde")); // false
+        System.out.println(validPalindrome("aba"));   // true
+        System.out.println(validPalindrome("abca"));  // true
+        System.out.println(validPalindrome("abc"));   // false
     }
 
-    private static boolean isSubsequence(String s, String t) {
-        int left = 0, right = 0;
-
-        while (left < s.length() && right < t.length()) {
-            if (s.charAt(left) == t.charAt(right)) {
-                left++;
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
             }
-            right++;
+
+            left++;
+            right--;
         }
 
-        return left == s.length();
+        return true;
+    }
+
+    private static boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
