42dca44dd36e81ef517bddcb66922e3be18f5695
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-10 08:13:50 +0300

    feat: 75. Sort Colors

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,36 +1,40 @@
 import java.util.Arrays;
 
-import twoPointers.SquaresOfASortedArray.SquaresOfASortedArray;
-
 public class Training {
     public static void main(String[] args) {
-        int[] nums = new int[]{-4, -1, 0, 3, 10};
-        // Создаем экземпляр класса для вызова нестатического метода
-        SquaresOfASortedArray solution = new SquaresOfASortedArray();
-        System.out.println(Arrays.toString(solution.sortedSquares(nums))); // [0,1,9,16,100]
+        int[] nums1 = {2, 0, 2, 1, 1, 0};
+        sortColors(nums1);
+        System.out.println(Arrays.toString(nums1)); // [0,0,1,1,2,2]
+
+        int[] nums2 = {2, 0, 1};
+        sortColors(nums2);
+        System.out.println(Arrays.toString(nums2)); // [0,1,2]
     }
 
-    private int[] sortedSquares(int[] nums) {
-        int n = nums.length; // Длина массива
-        int[] result = new int[n];
-        int left = 0; // Индекс первого элемента массива nums
-        int right = n - 1; // Индекс последнего элемента массива nums
-        int index = n - 1; // Индекс последнего элемента массива result
+        private static void sortColors(int[] nums) {
+        int i = 0; // Индекс текущего элемента
+        int left = 0, right = nums.length - 1; // Левая и правая границы
 
-        while (left <= right) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-
-            if (leftSquare > rightSquare) {
-                result[index] = leftSquare;
+        while (left < right && i <= right) { // пока левая граница меньше правой и индекс меньше правой границы
+            // Если текущий элемент равен 0, то меняем его местами с элементом в начале массива
+            if (nums[i] == 0) {
+                swap(nums, left, i);
                 left++;
-            } else {
-                result[index] = rightSquare;
+                i++;
+                // Если текущий элемент равен 2, то меняем его местами с элементом в конце массива
+            } else if (nums[i] == 2) {
+                swap(nums, right, i);
                 right--;
+                // Если текущий элемент равен 1, то оставляем его на месте и переходим к следующему элементу
+            } else {
+                i++;
             }
-            index--;
         }
+    }
 
-        return result;
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
