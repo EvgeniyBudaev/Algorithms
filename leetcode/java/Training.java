99aaaf923b4707c83925cb910b218546803d1cbf
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-14 06:57:10 +0300

    feat: checkForTarget

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,39 +1,24 @@
 
 public class Training {
     public static void main(String[] args) {
-        System.out.println(isOneEditDistance("ab", "acb")); // true
-        System.out.println(isOneEditDistance("ab", "ad")); // true
-        System.out.println(isOneEditDistance("cab", "ad")); // false
+        int[] nums = {1, 2, 4, 6, 8, 9, 14, 15};
+        System.out.println(checkForTarget(nums, 13)); // true
     }
 
-    private static boolean isOneEditDistance(String s, String t) {
-        int m = s.length(), n = t.length();
+    private static boolean checkForTarget(int[] nums, int target) {
+        int left = 0, right = nums.length - 1; // левая и правая границы массива
 
-        // Меняем местами строки, чтобы s всегда была длиннее строки t.
-        if (m < n) {
-            return isOneEditDistance(t, s);
-        }
-
-        // Если разница в длинах больше 1, то строки не могут быть на расстоянии одного редактирования друг от друга.
-        if (m - n > 1) {
-            return false;
-        }
-
-        // s: "acb", t: "ab"
-        for (int i = 0; i < n; i++) {
-            // Если символы строки s и t не совпадают, то проверяем, что строка s имеет длину n+1.
-            if (s.charAt(i) != t.charAt(i)) {
-                // Если строки имеют одинаковую длину, то проверяем, что строки равны, начиная с i+1 символа.
-                if (m == n) {
-                    return s.substring(i + 1).equals(t.substring(i + 1));
-                }
-                // Если строки имеют разную длину, то проверяем, что строка s имеет длину n+1.
-                // s[i+1:]: "b" t[i:]: "b
-                return s.substring(i + 1).equals(t.substring(i));
+        while (left < right) { // пока левая граница меньше правой
+            int sum = nums[left] + nums[right]; // сумма пары
+            if (sum == target) {
+                return true;
+            } else if (sum > target) { // если сумма больше целевого значения, то уменьшаем правую границу
+                right--;
+            } else { // если сумма меньше целевого значения, то увеличиваем левую границу
+                left++;
             }
         }
 
-        // Если все символы строки s и t совпадают, то проверяем, что строка s имеет длину n+1.
-        return m == n + 1;
+        return false; // если не найдено
     }
 }
