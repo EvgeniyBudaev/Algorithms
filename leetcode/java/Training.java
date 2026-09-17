0e015d340a7014d1f66ca05493c2b96b17039a27
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-17 07:33:31 +0300

    feat: TreeMap

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,9 +1,15 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Training {
     public static void main(String[] args) {
-        List<String> list = new LinkedList<>();
+        Map<Object, Object> map = new HashMap<>();
+        map.put(new Object(), new Object());
+        map.put(new Object(), new Object());
+        TreeMap<Object, Object> treeMap = new TreeMap<>();
+        treeMap.putAll(map);
+        System.out.println(treeMap.size());
     }
 }
 
