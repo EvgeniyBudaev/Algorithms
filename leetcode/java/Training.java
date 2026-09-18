04fe891c03dc7d45e912391882385167b9507792
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-18 16:08:15 +0300

    feat: what is the output?

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,15 +1,38 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class Training {
     public static void main(String[] args) {
-        Map<Object, Object> map = new HashMap<>();
-        map.put(new Object(), new Object());
-        map.put(new Object(), new Object());
-        TreeMap<Object, Object> treeMap = new TreeMap<>();
-        treeMap.putAll(map);
-        System.out.println(treeMap.size());
+        new Training().go();
     }
-}
 
+    void go() {
+        Mammal m = new Zebra();
+        System.out.println(m.name + " " + m.makeNoise() + " " + m.foo());
+
+        Zebra z = new Zebra();
+        System.out.println(z.name + " " + z.makeNoise() + " " + z.foo());
+    }
+
+    class Mammal {
+        String name = "furry";
+
+        String makeNoise() {
+            return "generic noise";
+        }
+
+        static String foo() {
+            return "foo";
+        }
+    }
+
+    class Zebra extends Mammal {
+        String name = "stripes";
+
+        String makeNoise() {
+            return "bray";
+        }
+
+        static String foo() {
+            return "baz";
+        }
+    }
+
+}
