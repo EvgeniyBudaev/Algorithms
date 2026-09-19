828033ee1afdabc331d2c2aa01744b8a733f15c7
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-19 20:42:02 +0300

    feat: FunctionalInterface

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,38 +1,17 @@
+
 public class Training {
     public static void main(String[] args) {
-        new Training().go();
+        String prefix = "Hello";
+
+        Greeting task = () -> {
+            System.out.println(prefix);
+        };
+
+        task.sayHello();
     }
 
-    void go() {
-        Mammal m = new Zebra();
-        System.out.println(m.name + " " + m.makeNoise() + " " + m.foo());
-
-        Zebra z = new Zebra();
-        System.out.println(z.name + " " + z.makeNoise() + " " + z.foo());
+    @FunctionalInterface
+    interface Greeting {
+        void sayHello();
     }
-
-    class Mammal {
-        String name = "furry";
-
-        String makeNoise() {
-            return "generic noise";
-        }
-
-        static String foo() {
-            return "foo";
-        }
-    }
-
-    class Zebra extends Mammal {
-        String name = "stripes";
-
-        String makeNoise() {
-            return "bray";
-        }
-
-        static String foo() {
-            return "baz";
-        }
-    }
-
 }
