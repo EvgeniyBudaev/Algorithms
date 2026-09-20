7d3e661d1d0fb94873ad411c7c21c0a09a78c5a2
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-20 18:27:37 +0300

    feat: Training

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,17 +1,6 @@
 
+
 public class Training {
     public static void main(String[] args) {
-        String prefix = "Hello";
-
-        Greeting task = () -> {
-            System.out.println(prefix);
-        };
-
-        task.sayHello();
-    }
-
-    @FunctionalInterface
-    interface Greeting {
-        void sayHello();
     }
 }
