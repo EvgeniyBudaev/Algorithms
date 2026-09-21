7e1fc928efc8744cb95a362b6e3b45e76457cc5b
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-21 19:14:03 +0300

    feat: CheckIfPalindrome

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -2,5 +2,21 @@
 
 public class Training {
     public static void main(String[] args) {
+        System.out.println(checkIfPalindrome("racecar")); // true
+        System.out.println(checkIfPalindrome("aleba")); // false
+    }
+
+    private static boolean checkIfPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
