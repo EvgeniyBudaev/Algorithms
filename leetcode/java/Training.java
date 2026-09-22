e96b217a2ebbc15983d1e4b43354bb629584e80f
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-22 09:42:27 +0300

    feat: Link reassignment

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -5,7 +5,9 @@ public class Training {
         Training t1 = new Training();
         Training t2 = new Training();
         Training r = t1.go(t2);
-        System.out.println(r); // null
+        System.out.println("t2 before: " + t2); // Training@2a139a55
+        System.out.println("r: " + r); // null
+        System.out.println("t2 after: " + t2); // Training@2a139a55
     }
 
     public Training go(Training tr) {
