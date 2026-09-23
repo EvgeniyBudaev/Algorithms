a155a6c98dc0cd32b4f2dc5bfc2db846efc07ef6
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-23 14:07:56 +0300

    feat: unboxing NullPointerException

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -2,16 +2,8 @@
 
 public class Training {
     public static void main(String[] args) {
-        Training t1 = new Training();
-        Training t2 = new Training();
-        Training r = t1.go(t2);
-        System.out.println("t2 before: " + t2); // Training@2a139a55
-        System.out.println("r: " + r); // null
-        System.out.println("t2 after: " + t2); // Training@2a139a55
-    }
-
-    public Training go(Training tr) {
-        tr = null;
-        return tr;
+        Float wrapper = null;
+        float primitive = wrapper; // NullPointerException
+        System.out.println(primitive); 
     }
 }
