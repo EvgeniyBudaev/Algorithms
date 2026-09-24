f0eb231978637fbd4b3e962903893998d2357447
Evgeniy Budaev <budaev.e@gmail.com>
2026-09-24 07:58:17 +0300

    feat: Comparator

--- leetcode/java/Training.java
+++ leetcode/java/Training.java
@@ -1,9 +1,20 @@
-
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Training {
     public static void main(String[] args) {
-        Float wrapper = null;
-        float primitive = wrapper; // NullPointerException
-        System.out.println(primitive); 
+        Comparator<Film> comparator = (i1, i2) -> i1.rating - i2.rating;
+        Set<Film> films = new TreeSet<>(comparator);
+
+        for (Film film : films) {
+            System.out.println(film);
+        }
     }
+
+    public class Film {
+        public String title;
+        public String directorName;
+        public int rating;
+    } 
 }
